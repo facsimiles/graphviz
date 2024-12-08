eb4c65ad8c0c4ed5181e63341173f68ab325cf51
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-08 13:15:14 +1100

    gd plugin gdgen_begin_page: condense 'bgcolor_str' usage and constify

--- plugin/gd/gvrender_gd.c
+++ plugin/gd/gvrender_gd.c
@@ -76,12 +76,11 @@ static int transparent, basecolor;
 
 static void gdgen_begin_page(GVJ_t * job)
 {
-    char *bgcolor_str = NULL;
     bool truecolor_p = false;	/* try to use cheaper paletted mode */
     gdImagePtr im = NULL;
 
     const char *const truecolor_str = agget(job->gvc->g, "truecolor"); // allow user to force truecolor
-    bgcolor_str = agget(job->gvc->g, "bgcolor");
+    const char *const bgcolor_str = agget(job->gvc->g, "bgcolor");
 
     if (truecolor_str && truecolor_str[0])
 	truecolor_p = mapbool(truecolor_str);
