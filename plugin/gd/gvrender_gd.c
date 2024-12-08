7e8c87c9c9b34e4213b02a9fade664a3dc02bc9c
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-08 13:15:14 +1100

    gd plugin gdgen_bezier: condense 'pen_ok' initialization and constify

--- plugin/gd/gvrender_gd.c
+++ plugin/gd/gvrender_gd.c
@@ -393,7 +393,7 @@ static void gdgen_bezier(GVJ_t *job, pointf *A, size_t n, int filled) {
     obj_state_t *obj = job->obj;
     gdImagePtr im = job->context;
     pointf V[4];
-    bool pen_ok, fill_ok;
+    bool fill_ok;
     gdImagePtr brush = NULL;
     gdPoint F[4];
 
@@ -401,7 +401,7 @@ static void gdgen_bezier(GVJ_t *job, pointf *A, size_t n, int filled) {
 	return;
 
     const int pen = gdgen_set_penstyle(job, im, &brush);
-    pen_ok = pen != gdImageGetTransparent(im);
+    const bool pen_ok = pen != gdImageGetTransparent(im);
     fill_ok = filled && obj->fillcolor.u.index != gdImageGetTransparent(im);
 
     if (pen_ok || fill_ok) {
