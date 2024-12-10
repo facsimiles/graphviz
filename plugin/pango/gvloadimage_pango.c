acb085d9d88e965d42e2ea5dadedb29343d9ab86
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-10 20:19:15 +1100

    pango plugin pango_loadimage_ps: condense initialization of 'surface'

--- plugin/pango/gvloadimage_pango.c
+++ plugin/pango/gvloadimage_pango.c
@@ -104,13 +104,12 @@ static void pango_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, bool fil
 
 static void pango_loadimage_ps(GVJ_t * job, usershape_t *us, boxf b, bool filled)
 {
-    cairo_surface_t *surface; 	/* source surface */
     cairo_format_t format;
 
     // suppress unused parameter warning
     (void)filled;
 
-    surface = cairo_loadimage(us);
+    cairo_surface_t *const surface = cairo_loadimage(us); // source surface
     if (surface) {
        	format = cairo_image_surface_get_format(surface);
         if ((format != CAIRO_FORMAT_ARGB32) && (format != CAIRO_FORMAT_RGB24))
