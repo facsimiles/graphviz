600fb44aa0f4de1491faf671de49622ad3cf920c
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-10 17:09:43 +1100

    neatogen majorization: condense 'margin' initialization and constify

--- lib/neatogen/neatoinit.c
+++ lib/neatogen/neatoinit.c
@@ -1118,11 +1118,6 @@ majorization(graph_t *mg, graph_t * g, int nv, int mode, int model, int dim, adj
     node_t *v;
     vtx_data *gp;
     node_t** nodes;
-#ifdef DIGCOLA
-#ifdef IPSEPCOLA
-    expand_t margin;
-#endif
-#endif
     int init = checkStart(g, nv, mode == MODE_HIER ? INIT_SELF : INIT_RANDOM);
     int opts = checkExp (g);
 
@@ -1184,7 +1179,7 @@ majorization(graph_t *mg, graph_t * g, int nv, int mode, int model, int dim, adj
                     fprintf(stderr,"Removing overlaps as postprocess...\n");
             }
             else opt.noverlap = 0;
-	    margin = sepFactor (g);
+	    const expand_t margin = sepFactor (g);
  	    /* Multiply by 2 since opt.gap is the gap size, not the margin */
 	    if (margin.doAdd) {
 		opt.gap.x = 2.0*PS2INCH(margin.x);
