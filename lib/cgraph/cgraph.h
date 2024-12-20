bdd818ba8322f846ef26d522f709f8098d48d18a
Stephen North <scnorth@gmail.com>
2024-12-20 09:01:18 -0500

    Fix C/C++ linkage by merging with current head

--- lib/cgraph/cgraph.h
+++ lib/cgraph/cgraph.h
@@ -51,25 +51,6 @@ extern "C" {
  * @{
  */
 
-/*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- *
- * Contributors: Details at https://graphviz.org
- *************************************************************************/
-
-#pragma once
-
-#include <inttypes.h>
-#include "cdt.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /// @cond
 
 #ifdef GVDLL
