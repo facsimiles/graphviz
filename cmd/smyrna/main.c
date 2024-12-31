facsimiles/graphviz

commit bace2961f4c60bad2757a542ea55bb715edd865c

smyrna find_me: avoid 'PATH_MAX'
Unconditional use of `PATH_MAX` is causing compilation failures on Hurd, where
`PATH_MAX` is not defined. Moreover there are some critical problems with
relying on `PATH_MAX`.¹ As glibc’s `readlink` man page notes:

  Using a statically sized buffer might not provide enough room for the symbolic
  link contents. The required size for the buffer can be obtained from the
  `stat.st_size` value returned by a call to `lstat(2)` on the link. However,
  the number of bytes written by `readlink()` and `readlinkat()` should be
  checked to make sure that the size of the symbolic link did not increase
  between the calls. Dynamically allocating the buffer for `readlink()` and
  `readlinkat()` also addresses a common portability problem when using
  `PATH_MAX` for the buffer size, as this constant is not guaranteed to be
  defined per POSIX if the system does not have such limit.

This commit is part of work to remove the use of `PATH_MAX`. Like
24bd92c1e5d49b354141cd06d88ca658991b9825 from where this code originated, these
changes are based on public domain code.²

Gitlab: #2452
Reported-by: László Böszörményi (GCS) <gcs@debian.org>

¹ https://insanecoding.blogspot.com/2007/11/pathmax-simply-isnt.html
² https://github.com/Smattr/clink/commit/5fe9c1236a45fbe92d5eb73361ccd4d2f6db748a

cmd/smyrna/main.c
@@ -185,18 +185,35 @@ static void windowedMode(int argc, char *argv[])
 }
 
 #if !defined(__APPLE__) && !defined(_WIN32)
-/// read the given symlink in the /proc file system
-static char *read_proc(const char *path) {
+/// `readlink`-alike but dynamically allocates
+static char *readln(const char *path) {
 
-  char buf[PATH_MAX + 1] = {0};
-  if (readlink(path, buf, sizeof(buf)) < 0)
-    return NULL;
+  char *resolved = NULL;
+  size_t size = 0;
 
-  // was the path too long?
-  if (buf[sizeof(buf) - 1] != '\0')
-    return NULL;
+  while (true) {
 
-  return gv_strdup(buf);
+    // expand target buffer
+    resolved = gv_realloc(resolved, size, size == 0 ? 1024 : (size * 2));
+    size = size == 0 ? 1024 : (size * 2);
+
+    // attempt to resolve
+    {
+      ssize_t written = readlink(path, resolved, size);
+      if (written < 0) {
+        break;
+      }
+      if ((size_t)written < size) {
+        // success
+        resolved[written] = '\0';
+        return resolved;
+      }
+    }
+  }
+
+  // failed
+  free(resolved);
+  return NULL;
 }
 #endif
 
@@ -222,12 +239,12 @@ static char *find_me(void) {
 
     // try to resolve any levels of symlinks if possible
     while (true) {
-      char buf[PATH_MAX + 1] = {0};
-      if (readlink(path, buf, sizeof(buf)) < 0)
+      char *buf = readln(path);
+      if (buf == NULL)
         return path;
 
       free(path);
-      path = gv_strdup(buf);
+      path = buf;
     }
   }
 #elif defined(_WIN32)
@@ -254,17 +271,17 @@ static char *find_me(void) {
 #else
 
   // Linux
-  char *path = read_proc("/proc/self/exe");
+  char *path = readln("/proc/self/exe");
   if (path != NULL)
     return path;
 
   // DragonFly BSD, FreeBSD
-  path = read_proc("/proc/curproc/file");
+  path = readln("/proc/curproc/file");
   if (path != NULL)
     return path;
 
   // NetBSD
-  path = read_proc("/proc/curproc/exe");
+  path = readln("/proc/curproc/exe");
   if (path != NULL)
     return path;
 
@@ -273,10 +290,24 @@ static char *find_me(void) {
   {
     int mib[] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
     static const size_t MIB_LENGTH = sizeof(mib) / sizeof(mib[0]);
-    char buf[PATH_MAX + 1] = {0};
-    size_t buf_size = sizeof(buf);
-    if (sysctl(mib, MIB_LENGTH, buf, &buf_size, NULL, 0) == 0)
-      return gv_strdup(buf);
+
+    do {
+      // determine how long the path is
+      size_t buf_size = 0;
+      if (sysctl(mib, MIB_LENGTH, NULL, &buf_size, NULL, 0) < 0) {
+        break;
+      }
+      assert(buf_size > 0);
+
+      // make enough space for the target path
+      char *buf = gv_alloc(buf_size);
+
+      // resolve it
+      if (sysctl(mib, MIB_LENGTH, buf, &buf_size, NULL, 0) == 0) {
+        return buf;
+      }
+      free(buf);
+    } while (0);
   }
 #endif
 #endif

commit b215dbe2cbcfa3e76e242a444cf69aac54a7a046

smyrna: fix macOS compilation
bace2961f4c60bad2757a542ea55bb715edd865c introduced a bug by calling `readln` in
the macOS path but only defining `readln` on non-Apple, non-Windows platforms.
This was not caught in CI because Smyrna is not built there on macOS due to
missing dependencies.

Gitlab: #2411, #2422, #2452, #2494, #2553, fixes #2631
Macports: https://trac.macports.org/ticket/71615
Reported-by: Eric Gallager
Reported-by: Ryan Schmidt <gitlab@ryandesign.com>

cmd/smyrna/main.c
@@ -178,7 +178,7 @@ static void windowedMode(int argc, char *argv[]) {
     gtk_main();
 }
 
-#if !defined(__APPLE__) && !defined(_WIN32)
+#ifndef _WIN32
 /// `readlink`-alike but dynamically allocates
 static char *readln(const char *path) {
 