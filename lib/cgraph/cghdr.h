facsimiles/graphviz

commit 41c218e33bfd9242625826b92293dd4f149eee22

cgraph: add a linear representation of subgraphs
As explained in the comments added in this commit, this representation is
significantly faster to operate on than the existing subgraph collection. On the
motivating example from #2593, the upcoming commits are able to leverage this
into a ~2× speed up.

This has a negligible effect on memory usage. On a subgraph-intensive example:¹

  ┌───────────────────┬───────────────────┬────────┐
  │      before       │       after       │  diff  │
  ├───────────────────┼───────────────────┼────────┤
  │ 1505864KB max rss │ 1516800KB max rss │ ~+0.7% │
  └───────────────────┴───────────────────┴────────┘

The way this new representation is plumbed into `Agraph_t` is quite unorthodox.
The intent behind this approach is to avoid an API break: existing code should
work as-is after this change. Whether taking an approach like this is worthwhile
remains to be seen.

¹ `ccomps ec_pairs-12.gv` using the graph from #2593.

Gitlab: #2593
Reported-by: Diomidis Spinellis

lib/cgraph/cghdr.h
@@ -31,6 +31,7 @@
 
 #include <cgraph.h>
 
+#include	 	<cgraph/list.h>
 #include	 	<ctype.h>
 #include		<sys/types.h>
 #include		<stdarg.h>
@@ -140,3 +141,40 @@ void aginitcb(Agraph_t * g, void *obj, Agcbstack_t * disc);
 void agupdcb(Agraph_t * g, void *obj, Agsym_t * sym, Agcbstack_t * disc);
 void agdelcb(Agraph_t * g, void *obj, Agcbstack_t * disc);
 /// @}
+
+DEFINE_LIST(Agraphs, Agraph_t *)
+
+/// two alternative representations of a graph collection
+///
+/// The subgraphs of a graph have traditionally been stored in a `Dt_t`. However
+/// it is much faster to iterate through a C array of the subgraphs instead of a
+/// cdt tree of them. To preserve backwards compatibility, we maintain both:
+///
+///   what the cgraph.h user sees:            what we see internally:
+///
+///      Agraph_t                                Agraph_t
+///     ┌─────────┐                             ┌─────────┐
+///     │ …       │                             │ …       │
+///     ├─────────┤    ┌───────────┐            ├─────────┤    ┌───────────┐
+///     │  g_seq ─┼───►│   Dt_t    │            │  g_seq ─┼───►│   Dt_t    │
+///     ├─────────┤    └───────────┘            ├─────────┤    ├───────────┤
+///     │ …       │                             │ …       │    │ Agraphs_t │
+///                                                            └───────────┘
+///
+/// To facilitate this, `g_seq2` can be used to access the second
+/// representation.
+typedef struct {
+  Dt_t legacy;      ///< the traditional tree of subgraphs
+  Agraphs_t linear; ///< the newer contiguous collection
+} g_seq_t;
+
+/// access the list of subgraphs of a graph
+///
+/// See `g_seq_t` for an extended explanation of this.
+///
+/// @param g The parent graph to inspect
+/// @return A generic list of the subgraphs
+static inline Agraphs_t *g_seq2(Agraph_t *g) {
+  g_seq_t *seq = (g_seq_t *)g->g_seq;
+  return &seq->linear;
+}

lib/cgraph/graph.c
@@ -74,6 +74,11 @@ Agraph_t *agopen1(Agraph_t * g)
     g->e_seq = agdtopen(g, g == agroot(g)? &Ag_mainedge_seq_disc : &Ag_subedge_seq_disc, Dttree);
     g->e_id = agdtopen(g, g == agroot(g)? &Ag_mainedge_id_disc : &Ag_subedge_id_disc, Dttree);
     g->g_seq = agdtopen(g, &Ag_subgraph_seq_disc, Dttree);
+
+    // `agdtopen` has allocated a `Dt_t`, which we now expand to host a
+    // `g_seq_t`. See `g_seq_t` for why we do this odd thing.
+    g->g_seq = gv_realloc(g->g_seq, sizeof(Dt_t), sizeof(g_seq_t));
+
     g->g_id = agdtopen(g, &Ag_subgraph_id_disc, Dttree);
 
     par = agparent(g);
@@ -82,6 +87,7 @@ Agraph_t *agopen1(Agraph_t * g)
 	assert((seq & SEQ_MASK) == seq && "sequence ID overflow");
 	AGSEQ(g) = seq & SEQ_MASK;
 	dtinsert(par->g_seq, g);
+	Agraphs_append(g_seq2(par), g);
 	dtinsert(par->g_id, g);
     }
     if (!par || par->desc.has_attrs)
@@ -122,6 +128,11 @@ int agclose(Agraph_t * g)
     if (agdtclose(g, g->e_id)) return FAILURE;
     assert(dtsize(g->e_seq) == 0);
     if (agdtclose(g, g->e_seq)) return FAILURE;
+
+    // needs to be done before closing `g_seq` because `agdtclose(g, g->g_seq)`
+    // deallocates the memory this is hosted in
+    assert(Agraphs_is_empty(g_seq2(g)));
+    Agraphs_free(g_seq2(g));
 
     assert(dtsize(g->g_seq) == 0);
     if (agdtclose(g, g->g_seq)) return FAILURE;

lib/cgraph/subg.c
@@ -97,6 +97,7 @@ Agraph_t *agparent(Agraph_t * g)
  */
 int agdelsubg(Agraph_t * g, Agraph_t * subg)
 {
+    Agraphs_remove(g_seq2(g), subg);
     return ( (dtdelete(g->g_seq, subg) != NULL)  &&
         (dtdelete(g->g_id, subg) != NULL) );
 }

commit 31716fdb056115ca6e47c4d1a047b218d185b63d

API BREAK: cgraph: extra 'g_seq2' into its own field
41c218e33bfd9242625826b92293dd4f149eee22 introduced a hidden member of
`Agraph_t` that could be located through some pointer addition on `g_seq`. This
was a cute trick to avoid an API break but involved code that was not very
clear. With the upcoming release due to be an API break, we can take the
opportunity to unwind this technical debt into something more obvious.

`void *` is used rather than a stronger type to avoid having to define
`Agraphs_t` in a public header. Users are not expected to access this field
directly.

Gitlab: #2593

lib/cgraph/cghdr.h
@@ -154,38 +154,3 @@ void agdelcb(Agraph_t * g, void *obj, Agcbstack_t * disc);
 /// @}
 
 DEFINE_LIST(Agraphs, Agraph_t *)
-
-/// two alternative representations of a graph collection
-///
-/// The subgraphs of a graph have traditionally been stored in a `Dt_t`. However
-/// it is much faster to iterate through a C array of the subgraphs instead of a
-/// cdt tree of them. To preserve backwards compatibility, we maintain both:
-///
-///   what the cgraph.h user sees:            what we see internally:
-///
-///      Agraph_t                                Agraph_t
-///     ┌─────────┐                             ┌─────────┐
-///     │ …       │                             │ …       │
-///     ├─────────┤    ┌───────────┐            ├─────────┤    ┌───────────┐
-///     │  g_seq ─┼───►│   Dt_t    │            │  g_seq ─┼───►│   Dt_t    │
-///     ├─────────┤    └───────────┘            ├─────────┤    ├───────────┤
-///     │ …       │                             │ …       │    │ Agraphs_t │
-///                                                            └───────────┘
-///
-/// To facilitate this, `g_seq2` can be used to access the second
-/// representation.
-typedef struct {
-  Dt_t legacy;      ///< the traditional tree of subgraphs
-  Agraphs_t linear; ///< the newer contiguous collection
-} g_seq_t;
-
-/// access the list of subgraphs of a graph
-///
-/// See `g_seq_t` for an extended explanation of this.
-///
-/// @param g The parent graph to inspect
-/// @return A generic list of the subgraphs
-static inline Agraphs_t *g_seq2(Agraph_t *g) {
-  g_seq_t *seq = (g_seq_t *)g->g_seq;
-  return &seq->linear;
-}

lib/cgraph/cgraph.h
@@ -428,8 +428,13 @@ struct Agraph_s {
   struct graphviz_node_set *n_id; ///< the node set indexed by ID
   Dict_t *e_seq, *e_id;           ///< holders for edge sets
   Dict_t *g_seq, *g_id;           ///< subgraphs - descendants
-  Agraph_t *parent, *root;        ///< subgraphs - ancestors
-  Agclos_t *clos;                 ///< shared resources
+  /// alternative subgraphs representation
+  ///
+  /// A C array of the subgraphs for faster iteration. This is only expected to
+  /// be accessed by libcgraph internally.
+  void *g_seq2;
+  Agraph_t *parent, *root; ///< subgraphs - ancestors
+  Agclos_t *clos;          ///< shared resources
 };
 
 /* graphs */

lib/cgraph/graph.c
@@ -74,10 +74,7 @@ Agraph_t *agopen1(Agraph_t * g)
     g->e_seq = agdtopen(g, g == agroot(g)? &Ag_mainedge_seq_disc : &Ag_subedge_seq_disc, Dttree);
     g->e_id = agdtopen(g, g == agroot(g)? &Ag_mainedge_id_disc : &Ag_subedge_id_disc, Dttree);
     g->g_seq = agdtopen(g, &Ag_subgraph_seq_disc, Dttree);
-
-    // `agdtopen` has allocated a `Dt_t`, which we now expand to host a
-    // `g_seq_t`. See `g_seq_t` for why we do this odd thing.
-    g->g_seq = gv_realloc(g->g_seq, sizeof(Dt_t), sizeof(g_seq_t));
+    g->g_seq2 = gv_alloc(sizeof(Agraphs_t));
 
     g->g_id = agdtopen(g, &Ag_subgraph_id_disc, Dttree);
 
@@ -87,7 +84,7 @@ Agraph_t *agopen1(Agraph_t * g)
 	assert((seq & SEQ_MASK) == seq && "sequence ID overflow");
 	AGSEQ(g) = seq & SEQ_MASK;
 	dtinsert(par->g_seq, g);
-	Agraphs_append(g_seq2(par), g);
+	Agraphs_append(par->g_seq2, g);
 	dtinsert(par->g_id, g);
     }
     if (!par || par->desc.has_attrs)
@@ -129,10 +126,9 @@ int agclose(Agraph_t * g)
     assert(dtsize(g->e_seq) == 0);
     if (agdtclose(g, g->e_seq)) return FAILURE;
 
-    // needs to be done before closing `g_seq` because `agdtclose(g, g->g_seq)`
-    // deallocates the memory this is hosted in
-    assert(Agraphs_is_empty(g_seq2(g)));
-    Agraphs_free(g_seq2(g));
+    assert(Agraphs_is_empty(g->g_seq2));
+    Agraphs_free(g->g_seq2);
+    free(g->g_seq2);
 
     assert(dtsize(g->g_seq) == 0);
     if (agdtclose(g, g->g_seq)) return FAILURE;

lib/cgraph/subg.c
@@ -98,7 +98,7 @@ Agraph_t *agparent(Agraph_t * g)
  */
 int agdelsubg(Agraph_t * g, Agraph_t * subg)
 {
-    Agraphs_remove(g_seq2(g), subg);
+    Agraphs_remove(g->g_seq2, subg);
     return ( (dtdelete(g->g_seq, subg) != NULL)  &&
         (dtdelete(g->g_id, subg) != NULL) );
 }

lib/cgraph/write.c
@@ -441,7 +441,7 @@ static bool irrelevant_subgraph(Agraph_t * g)
 
 static bool node_in_subg(Agraph_t * g, Agnode_t * n)
 {
-    Agraphs_t *subgs = g_seq2(g);
+    Agraphs_t *subgs = g->g_seq2;
 
     for (size_t i = 0; i < Agraphs_size(subgs); ++i) {
 	Agraph_t *subg = Agraphs_get(subgs, i);
@@ -647,7 +647,7 @@ static int write_port(Agedge_t * e, iochan_t * ofile, Agsym_t * port)
 }
 
 static bool write_edge_test(Agraph_t *g, Agedge_t *e) {
-    Agraphs_t *subgs = g_seq2(g);
+    Agraphs_t *subgs = g->g_seq2;
 
     /* can use agedge() because we subverted the dict compar_f */
     for (size_t i = 0; i < Agraphs_size(subgs); ++i) {