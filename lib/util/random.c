e569c7bbbba834b9ec5ba2c78a4289c9c6e45175
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-03-07 18:47:15 -0800

    util gv_random: fix handling of bounds > 'RAND_MAX'
    `RAND_MAX` is only guaranteed to be at least 32767. So attempting to generate a
    random number in a range that exceeded this could result in high values never
    being generated. This is not hypothetical. One of the two existing `gv_random`
    calls passes 100000.
    
    This change allows `gv_random` to support the entire range of `int`.

--- lib/util/random.c
+++ lib/util/random.c
@@ -2,11 +2,16 @@
 /// @brief Implementation of random number generation functionality
 
 #include <assert.h>
+#include <limits.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <util/random.h>
 
-int gv_random(int bound) {
+/// handle random number generation, `bound ≤ RAND_MAX`
+static int random_small(int bound) {
   assert(bound > 0);
+  assert(bound <= RAND_MAX);
 
   // The interval `[0, RAND_MAX]` is not necessarily neatly divided into
   // `bound`-sized chunks. E.g. using a bound of 3 with a `RAND_MAX` of 7:
@@ -18,11 +23,8 @@ int gv_random(int bound) {
   // To guarantee a uniform distribution, derive the upper bound of the last
   // complete chunk (5 in the example above), above which we discard and
   // resample to avoid pulling from the partial trailing chunk.
-  int discard_threshold = RAND_MAX;
-  if (bound <= RAND_MAX) {
-    discard_threshold =
-        RAND_MAX - (int)(((unsigned)RAND_MAX + 1) % (unsigned)bound);
-  }
+  const int discard_threshold =
+      RAND_MAX - (int)(((unsigned)RAND_MAX + 1) % (unsigned)bound);
 
   int r;
   do {
@@ -30,4 +32,43 @@ int gv_random(int bound) {
   } while (r > discard_threshold);
 
   return r % bound;
+}
+
+/// handle random number generation, `bound > RAND_MAX`
+static int random_big(int bound) {
+  assert(bound > 0);
+
+  // see comment in `random_small`, but note that our maximum generated value
+  // here will be `INT_MAX` instead of `RAND_MAX`
+  const int discard_threshold =
+      INT_MAX - (int)(((unsigned)INT_MAX + 1) % (unsigned)bound);
+
+  int r;
+  do {
+    // generate a random `sizeof(int) * CHAR_BIT`-bit wide value
+    unsigned raw = 0;
+    for (size_t i = 0; i < sizeof(int); ++i) {
+      // `RAND_MAX ≥ 32767` is guaranteed, so `random_small(256)` is safe
+      const uint8_t byte = (uint8_t)random_small((int)UINT8_MAX + 1);
+      memcpy((char *)&raw + i, &byte, sizeof(byte));
+    }
+
+    // Shift out the sign bit to force a non-negative value. Assumes two’s
+    // complement representation.
+    const unsigned natural = raw << 1 >> 1;
+
+    r = (int)natural;
+
+  } while (r > discard_threshold);
+
+  return r % bound;
+}
+
+int gv_random(int bound) {
+  assert(bound > 0);
+
+  if (bound > RAND_MAX) {
+    return random_big(bound);
+  }
+  return random_small(bound);
 }
