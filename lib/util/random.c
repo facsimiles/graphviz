25ab8ec367ae87f7f4e8d0606ad6e236d8de44f1
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-03-07 18:47:15 -0800

    util gv_random: fix non-uniformity of distribution
    When random number generation is used for something like a Fisher-Yates
    shuffle,¹ as it is for the call in `random_permutation`, it is important that
    the generated numbers are uniformly distributed. Wikipedia explains a common
    mistake² that the implementation prior to this change suffered from:
    
      Most random number generators, however — whether true or pseudorandom — will
      only directly provide numbers in a fixed range from 0 to RAND_MAX, and in some
      libraries, RAND_MAX may be as low as 32767. A simple and commonly used way to
      force such numbers into a desired range is to apply the modulo operator; that
      is, to divide them by the size of the range and take the remainder. However,
      the need in a Fisher–Yates shuffle to generate random numbers in every range
      from 0–1 to 0–n almost guarantees that some of these ranges will not evenly
      divide the natural range of the random number generator. Thus, the remainders
      will not always be evenly distributed and, worse yet, the bias will be
      systematically in favor of small remainders.
    
      For example, assume that your random number source gives numbers from 0 to 99
      (as was the case for Fisher and Yates' original tables), which is 100 values,
      and that you wish to obtain an unbiased random number from 0 to 15 (16
      values). If you simply divide the numbers by 16 and take the remainder, you
      will find that the numbers 0–3 occur about 17% more often than others. This is
      because 16 does not evenly divide 100: the largest multiple of 16 less than or
      equal to 100 is 6×16 = 96, and it is the numbers in the incomplete range 96–99
      that cause the bias.
    
    The change in this commit implements the fix suggested by the same Wikipedia
    page:
    
      The simplest way to fix the problem is to discard those numbers before taking
      the remainder and to keep trying again until a number in the suitable range
      comes up. While in principle this could, in the worst case, take forever, the
      expected number of retries will always be less than one.
    
    ¹ https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
    ² https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modulo_bias

--- lib/util/random.c
+++ lib/util/random.c
@@ -7,5 +7,27 @@
 
 int gv_random(int bound) {
   assert(bound > 0);
-  return rand() % bound;
+
+  // The interval `[0, RAND_MAX]` is not necessarily neatly divided into
+  // `bound`-sized chunks. E.g. using a bound of 3 with a `RAND_MAX` of 7:
+  //   ┌───┬───┬───┬───┬───┬───┬───┬───┐
+  //   │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │
+  //   └───┴───┴───┴───┴───┴───┴───┴───┘
+  //    ◄─────────► ◄─────────► ◄─────►
+  //     3 values    3 values   2 values
+  // To guarantee a uniform distribution, derive the upper bound of the last
+  // complete chunk (5 in the example above), above which we discard and
+  // resample to avoid pulling from the partial trailing chunk.
+  int discard_threshold = RAND_MAX;
+  if (bound <= RAND_MAX) {
+    discard_threshold =
+        RAND_MAX - (int)(((unsigned)RAND_MAX + 1) % (unsigned)bound);
+  }
+
+  int r;
+  do {
+    r = rand();
+  } while (r > discard_threshold);
+
+  return r % bound;
 }
