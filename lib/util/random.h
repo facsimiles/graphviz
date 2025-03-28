e6f0dc29be477ae0b904a52fc61d232e0ccdc27c
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-03-27 20:12:08 -0700

    util: support for generating random 64-bit numbers
    This will be used in an upcoming commit.

--- lib/util/random.h
+++ lib/util/random.h
@@ -19,6 +19,8 @@
 #endif
 #endif
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -40,6 +42,15 @@ UTIL_API int *gv_permutation(int bound);
 /// @param bound Exclusive upper bound on random number generation
 /// @return A random number drawn from a uniform distribution
 UTIL_API int gv_random(int bound);
+
+/// generate a random 64-bit unsigned number in the range `[0, bound - 1]`
+///
+/// This function assumes the caller has previously seeded the `rand` random
+/// number generator.
+///
+/// @param bound Exclusive upper bound on random number generation
+/// @return A random number drawn from a uniform distribution
+UTIL_API uint64_t gv_random_u64(uint64_t bound);
 
 #ifdef __cplusplus
 }
