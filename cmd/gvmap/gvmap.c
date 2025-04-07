evilkim/gitlab-graphviz-mirror

commit a889d69258763de3dcaf267e584204eefcbb7fd3

gvmap: remove dynamic allocation for opacity
After the prior type signature changes, this string no longer needs to be
dynamically allocated. This also fixes a memory leak that would occur when
passing the -c command line argument multiple times.

cmd/gvmap/gvmap.c
@@ -45,7 +45,7 @@ typedef struct {
     int color_scheme;
     real line_width;
     char *color_scheme_str;
-    char *opacity;
+    const char *opacity;
     char *plot_label;
     real *bg_color;
     int improve_contiguity_n;
@@ -254,7 +254,7 @@ init(int argc, char **argv, params_t* pm)
       break;
     case 'c':
       if (sscanf(optarg,"_opacity=%2s", stmp) > 0 && strlen(stmp) == 2){
-        pm->opacity = strdup(stmp);
+        pm->opacity = stmp;
       } else if ((sscanf(optarg,"%d",&r) > 0) && r >= COLOR_SCHEME_NONE && r <= COLOR_SCHEME_GREY){
         pm->color_scheme = r;
       } else if (knownColorScheme(optarg)) {

facsimiles/graphviz

commit e781546801d9122f3a662d99d1f883fc764e5cd3

gvmap: fix retention of stack pointer beyond its lifetime
cppcheck observes:

  $ cppcheck -Ilib --check-level=exhaustive --force cmd/gvmap/gvmap.c
  cmd/gvmap/gvmap.c:238:9: error: Address of local auto-variable assigned to a
    function parameter. [autoVariables]
          pm->opacity = stmp;
          ^

This bug was introduced in a889d69258763de3dcaf267e584204eefcbb7fd3. No
adjustment needs to be made to follow on code because `opacity` is only ever
used as `if (opacity && strlen(opacity) >= 2) …`.

Reported-by: cppcheck 2.17.1

cmd/gvmap/gvmap.c
@@ -51,7 +51,7 @@ typedef struct {
     int color_scheme;
     double line_width;
     char *color_scheme_str;
-    const char *opacity;
+    char opacity[3];
     int improve_contiguity_n;
     int nart;
     bool color_optimize;
@@ -141,7 +141,7 @@ init(int argc, char **argv, params_t* pm)
   char stmp[3];  /* two character string plus '\0' */
 
   pm->outfile = NULL;
-  pm->opacity = NULL;
+  pm->opacity[0] = '\0';
   pm->color_scheme_str = NULL;
   pm->nrandom = -1;
   pm->dim = 2;
@@ -235,7 +235,7 @@ init(int argc, char **argv, params_t* pm)
       break;
     case 'c':
       if (sscanf(optarg,"_opacity=%2s", stmp) > 0 && strlen(stmp) == 2){
-        pm->opacity = stmp;
+        strncpy(pm->opacity, stmp, sizeof(pm->opacity));
       } else if (sscanf(optarg, "%d", &r) > 0 && r >= COLOR_SCHEME_NONE &&
                  r <= COLOR_SCHEME_GREY) {
         pm->color_scheme = r;