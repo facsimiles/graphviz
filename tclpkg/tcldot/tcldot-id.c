1d888a37060b64c6d36ece01ab5a81a31c05a2f8
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-04-29 18:15:49 +1000

    tcldot myiddisc_open: remove assumption that 'mydisc' is 'ictx_t'’s first member
    In combination with the previous commit, this makes the `ictx_t`↔`Agdisc_t`
    conversion robust against internal changes to the `ictx_t` layout.

--- tclpkg/tcldot/tcldot-id.c
+++ tclpkg/tcldot/tcldot-id.c
@@ -9,6 +9,7 @@
  *************************************************************************/
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <util/alloc.h>
 #include <util/unreachable.h>
@@ -16,7 +17,7 @@
 
 // Agiddisc functions
 static void *myiddisc_open(Agraph_t *g, Agdisc_t *disc) {
-    ictx_t *const ictx = (ictx_t *)disc;
+    ictx_t *const ictx = (ictx_t *)((char *)disc - offsetof(ictx_t, mydisc));
     gctx_t *const gctx = gv_alloc(sizeof(gctx_t));
     gctx->g = g;
     gctx->ictx = ictx;
