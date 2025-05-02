ellson/MOTHBALLED-graphviz

commit 1eda95f7f0f13f1135f73c30b3aa6f5f2f0982e7

fix deletes

tclpkg/tcldot/tcldot-util.c
@@ -64,7 +64,11 @@ Agedge_t *cmd2e(gctx_t *gctx, char *cmd) {
 #ifdef WITH_CGRAPH
 void deleteEdge(gctx_t *gctx, Agraph_t * g, Agedge_t *e)
 {
+    char buf[32];
+
+    sprintf(buf,"edge%p",e);
     agdelete(gctx->g, e);  /* delete edge from root graph */
+    Tcl_DeleteCommand(gctx->ictx->interp, buf);
 }
 static void deleteNodeEdges(gctx_t *gctx, Agraph_t *g, Agnode_t *n)
 {
@@ -79,8 +83,13 @@ static void deleteNodeEdges(gctx_t *gctx, Agraph_t *g, Agnode_t *n)
 }
 void deleteNode(gctx_t * gctx, Agraph_t *g, Agnode_t *n)
 {
+    char buf[32];
+
     deleteNodeEdges(gctx, gctx->g, n); /* delete all edges to/from node in root graph */
+
+    sprintf(buf,"node%p",n);
     agdelete(gctx->g, n); /* delete node from root graph */
+    Tcl_DeleteCommand(gctx->ictx->interp, buf);
 }
 static void deleteGraphNodes(gctx_t * gctx, Agraph_t *g)
 {
@@ -96,16 +105,20 @@ static void deleteGraphNodes(gctx_t * gctx, Agraph_t *g)
 void deleteGraph(gctx_t * gctx, Agraph_t *g)
 {
     Agraph_t *sg;
+    char buf[32];
 
     for (sg = agfstsubg (g); sg; sg = agnxtsubg (sg)) {
 	deleteGraph(gctx, sg);
     }
     deleteGraphNodes(gctx, g);
+
+    sprintf(buf,"graph%p",g);
     if (g == agroot(g)) {
 	agclose(g);
     } else {
 	agdelsubg(agroot(g), g);
     }
+    Tcl_DeleteCommand(gctx->ictx->interp, buf);
 }
 #else
 void deleteEdge(ictx_t * ictx, Agraph_t * g, Agedge_t *e)

facsimiles/graphviz

commit 5c6137ad6c54bdd477e25c01b2a9369c02056d9d

tcldot deleteGraph: fix use after free of graph context
Address Sanitizer observes:

  $ env LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.6 \
        LD_LIBRARY_PATH=${PREFIX}/lib \
        TCLLIBPATH=${PREFIX}/lib/graphviz/tcl \
        bash graphviz/tclpkg/tcldot/demo/doted.tcl \
        graphviz/tests/graphs/arrows.gv
  =================================================================
  ==42926==ERROR: AddressSanitizer: heap-use-after-free on address
    0x603000051918 at pc 0x7ffff3a21a13 bp 0x7fffffffd7b0 sp 0x7fffffffd7a0
  READ of size 8 at 0x603000051918 thread T0
      #0 0x7ffff3a21a12 in deleteGraph tclpkg/tcldot/tcldot-util.c:138
      #1 0x7ffff3a12f00 in graphcmd_internal tclpkg/tcldot/tcldot-graphcmd.c:123
      #2 0x7ffff3a1a6da in graphcmd tclpkg/tcldot/tcldot-graphcmd.c:537
      #3 0x7ffff728ae54 in TclInvokeStringCommand (/lib/x86_64-linux-gnu/libtcl8.6.so+0x3ae54)
      #4 0x7ffff728cd31 in TclNRRunCallbacks (/lib/x86_64-linux-gnu/libtcl8.6.so+0x3cd31)
      #5 0x7ffff728e027  (/lib/x86_64-linux-gnu/libtcl8.6.so+0x3e027)
      #6 0x7ffff73384f8 in Tcl_FSEvalFileEx (/lib/x86_64-linux-gnu/libtcl8.6.so+0xe84f8)
      #7 0x7ffff7e8873a in Tk_MainEx (/lib/x86_64-linux-gnu/libtk8.6.so+0x5873a)
      #8 0x555555555162  (/usr/bin/wish8.6+0x1162)
      #9 0x7ffff7029d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58
      #10 0x7ffff7029e3f in __libc_start_main_impl ../csu/libc-start.c:392
      #11 0x555555555194 in _start (/usr/bin/wish8.6+0x1194)

The problem identified is that the graph context, `gctx_t *`, is the ID
discipline allocated by and stored within the graph itself. So closing the
graph, `agclose(g)`, calls `free` on the context due to this being the
destructor in `myiddisc`. This bug was introduced in
1eda95f7f0f13f1135f73c30b3aa6f5f2f0982e7.

This fix swaps the order of operations, so that the graph context is only ever
accessed while it is still live. Curiously the pre-cgraph implementation

tclpkg/tcldot/tcldot-util.c
@@ -118,12 +118,12 @@ void deleteGraph(gctx_t * gctx, Agraph_t *g)
     deleteGraphNodes(gctx, g);
 
     char *const hndl = obj2cmd(g);
+    Tcl_DeleteCommand(gctx->ictx->interp, hndl);
     if (g == agroot(g)) {
 	agclose(g);
     } else {
 	agdelsubg(agroot(g), g);
     }
-    Tcl_DeleteCommand(gctx->ictx->interp, hndl);
 }
 
 static void myagxset(void *obj, Agsym_t *a, const char *val) {