facsimiles/graphviz

commit 7aaad845457e1f8c93ae07fa0a0ee4961f4242e6

expr: support customizing the array length operator ('#')
This is a step towards supporting `#ARGV`. Though `ARGV` is an array, libexpr is
not actually aware of it. It is a global defined by libgvpr, outside of the
context passed to libexpr to evaluate. Because of the dependency direction
(libgvpr depends on libexpr, rather than the other way around), libexpr cannot
simply check for or reference `ARGV`. Instead we need to thread accesses like
`#ARGV` through callbacks into libgvpr.

Gitlab: #2582

lib/expr/exeval.c
@@ -1327,7 +1327,15 @@ static Extype_t eval(Expr_t *ex, Exnode_t *exnode, void *env) {
 		}
 		return v;
 	case '#':
-		v.integer = dtsize(exnode->data.variable.symbol->local);
+		if (exnode->data.variable.symbol->index > 0) {
+			if (ex->disc->lengthf != NULL) {
+				v = ex->disc->lengthf(exnode->data.variable.symbol, ex->disc);
+			} else {
+				exerror("%s: cannot get length", x->data.variable.symbol->name);
+			}
+		} else {
+			v.integer = dtsize(exnode->data.variable.symbol->local);
+		}
 		return v;
 	case IN_OP:
 		v.integer = evaldyn (ex, exnode, env, 0);

lib/expr/expr.h
@@ -188,6 +188,16 @@ struct Exdisc_s				/* discipline			*/
 					/* reference value function	*/
 	int		(*setf)(Expr_t*, Exnode_t*, Exid_t*, Exref_t*, void*, Extype_t);
 					/* set value function		*/
+	/// length function
+	///
+	/// This callback allows a user-defined '#' handler. That is, extending the
+	/// array length operator’s function.
+	///
+	/// @param rhs The right-hand side operand. That is 'foo' in the expression
+	///   '# foo'.
+	/// @param disc Pointer to the containing discipline.
+	/// @return The “length”, according to the user’s interpretation.
+	Extype_t	(*lengthf)(Exid_t *rhs, Exdisc_t *disc);
 	/* exit function           */
 	Exexit_f	exitf;
 	int*		types;

commit e486eea0b4478943fc23e8187b957681da3b7a88

expr: squash macOS compilation warning
The macOS+CMake job has begun failing with:

    [ 29%] Building C object lib/expr/CMakeFiles/expr.dir/exeval.c.o
    /Users/gitlab/builds/graphviz/graphviz/lib/expr/exeval.c:1337:8: error: variable 'v' is used uninitialized whenever 'if' condition is false [-Werror,-Wsometimes-uninitialized]
                            if (ex->disc->lengthf != NULL) {
                                ^~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/gitlab/builds/graphviz/graphviz/lib/expr/exeval.c:1345:10: note: uninitialized use occurs here
                    return v;
                           ^
    /Users/gitlab/builds/graphviz/graphviz/lib/expr/exeval.c:1337:4: note: remove the 'if' if its condition is always true
                            if (ex->disc->lengthf != NULL) {
                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/gitlab/builds/graphviz/graphviz/lib/expr/exeval.c:1056:2: note: variable 'v' is declared here
            Extype_t                v;
            ^

This problem was introduced in 7aaad845457e1f8c93ae07fa0a0ee4961f4242e6.

Gitlab: fixes #2680

lib/expr/exeval.c
@@ -1338,6 +1338,7 @@ static Extype_t eval(Expr_t *ex, Exnode_t *exnode, void *env) {
 				v = ex->disc->lengthf(exnode->data.variable.symbol, ex->disc);
 			} else {
 				exerror("%s: cannot get length", x->data.variable.symbol->name);
+				v.integer = 0;
 			}
 		} else {
 			v.integer = dtsize(exnode->data.variable.symbol->local);