9b1888dba2ceefbf389af14455c2f4ee888ba41b
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-05-25 09:07:36 -0700

    GDK plugin: propagate DPI to output
    Gitlab: closes #1617
    Reported-by: Matthew Galati

--- plugin/gdk/gvdevice_gdk.c
+++ plugin/gdk/gvdevice_gdk.c
@@ -14,6 +14,7 @@
 #include <limits.h>
 #include <util/gv_math.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
+#include <util/agxbuf.h>
 
 enum {
 	FORMAT_BMP,
@@ -60,8 +61,17 @@ static void gdk_format(GVJ_t * job)
                 NULL                    // destroy_fn_data
                );
 
-    gdk_pixbuf_save_to_callback(pixbuf, writer, job, format_str, NULL, NULL);
+    agxbuf x_dpi = {0};
+    agxbprint(&x_dpi, "%.0f", job->dpi.x);
+    agxbuf y_dpi = {0};
+    agxbprint(&y_dpi, "%.0f", job->dpi.y);
 
+    gdk_pixbuf_save_to_callback(pixbuf, writer, job, format_str, NULL, "x-dpi",
+                                agxbuse(&x_dpi), "y-dpi", agxbuse(&y_dpi),
+                                NULL);
+
+    agxbfree(&y_dpi);
+    agxbfree(&x_dpi);
     g_object_unref(pixbuf);
 }
 
