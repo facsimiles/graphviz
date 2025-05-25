2e822ef3eb295339aa54051c12e9bc91bb23dbb2
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-05-25 09:44:31 -0700

    glcomp glCompCreatePangoTexture: take 'txt' as a const

--- lib/glcomp/glpangofont.c
+++ lib/glcomp/glpangofont.c
@@ -48,8 +48,9 @@ static PangoLayout *get_pango_layout(const char *markup_text,
 }
 
 unsigned char *glCompCreatePangoTexture(char *fontdescription, int fontsize,
-                                        char *txt, cairo_surface_t **surface,
-                                        int *w, int *h) {
+                                        const char *txt,
+                                        cairo_surface_t **surface, int *w,
+                                        int *h) {
     PangoLayout *layout;
     double width, height;
     *surface = NULL;
