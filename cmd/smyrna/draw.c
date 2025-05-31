facsimiles/graphviz

commit 2d10d171bbeccf1c57370c86f051f4805129de9c

smyrna: replace duplicated 'xml_string' with 'xml_escape'
This completes the long journey of unifying four XML escaping routines into one.

Closes #1868.

cmd/smyrna/draw.c
@@ -15,9 +15,12 @@ XDOT DRAWING FUNCTIONS, maybe need to move them somewhere else
 */
 #include "draw.h"
 #include <common/colorprocs.h>
+#include <common/types.h>
+#include <common/utils.h>
 #include "smyrna_utils.h"
 #include <glcomp/glutils.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include <xdot/xdot.h>
 #include "viewport.h"
@@ -320,6 +323,18 @@ static void InsertImage(sdot_op * o, int param)
     }
 }
 
+// see usage in EmbedText
+static int put(void *buffer, const char *s) {
+  char **b = buffer;
+
+  for (; *s != '\0'; ++s) {
+    **b = *s;
+    ++(*b);
+  }
+
+  return 0;
+}
+
 static void EmbedText(sdot_op* o, int param)
 {
 	(void)param;
@@ -343,12 +358,22 @@ static void EmbedText(sdot_op* o, int param)
 	y=o->op.u.text.y;
 	if (!o->font)
 	{
-		o->font=glNewFont(
-		view->widgets,
-		xml_string (o->op.u.text.text),
-		&view->penColor,
-		pangotext,
-		font_op->op.u.font.name,font_op->op.u.font.size,0);
+		// allocate a buffer large enough to hold the maximum escaped version of the
+		// text
+		char *escaped = calloc(sizeof(char), strlen(o->op.u.text.text) *
+		                                     sizeof("&#xFFFFFFFF;") + 1);
+		if (escaped == NULL)
+		  return;
+
+		// XML-escape the text
+		const xml_flags_t flags = {.dash = 1, .nbsp = 1};
+		char **ptr = &escaped;
+		(void)xml_escape(o->op.u.text.text, flags, put, ptr);
+
+		o->font = glNewFont(view->widgets, escaped, &view->penColor, pangotext,
+		                    font_op->op.u.font.name, font_op->op.u.font.size, 0);
+
+		free(escaped);
 	}
 	glCompDrawText3D(o->font,x,y,view->Topview->global_z,o->op.u.text.width,font_op->op.u.font.size);
 

cmd/smyrna/smyrna_utils.c
@@ -8,108 +8,9 @@
  * Contributors: Details at https://graphviz.org
  *************************************************************************/
 #include "smyrna_utils.h"
-#include <common/memory.h>
 #include <common/types.h>
 #include <common/utils.h>
 #include <limits.h>
-
-/* return true if *s points to &[A-Za-z]+;      (e.g. &Ccedil; )
- *                          or &#[0-9]*;        (e.g. &#38; )
- *                          or &#x[0-9a-fA-F]*; (e.g. &#x6C34; )
- */
-static int xml_isentity(char *s)
-{
-    s++;			/* already known to be '&' */
-    if (*s == ';') { // '&;' is not a valid entity
-	return 0;
-    }
-    if (*s == '#') {
-	s++;
-	if (*s == 'x' || *s == 'X') {
-	    s++;
-	    while ((*s >= '0' && *s <= '9')
-		   || (*s >= 'a' && *s <= 'f')
-		   || (*s >= 'A' && *s <= 'F'))
-		s++;
-	} else {
-	    while (*s >= '0' && *s <= '9')
-		s++;
-	}
-    } else {
-	while ((*s >= 'a' && *s <= 'z')
-	       || (*s >= 'A' && *s <= 'Z'))
-	    s++;
-    }
-    if (*s == ';')
-	return 1;
-    return 0;
-}
-
-char *xml_string(char *s)
-{
-    static char *buf = NULL;
-    static int bufsize = 0;
-    char *p, *sub, *prev = NULL;
-    int len, pos = 0;
-
-    if (!buf) {
-	bufsize = 64;
-	buf = N_NEW(bufsize, char);
-    }
-
-    p = buf;
-    while (s && *s) {
-	if (pos > (bufsize - 8)) {
-	    bufsize *= 2;
-	    buf = realloc(buf, bufsize);
-	    p = buf + pos;
-	}
-	/* escape '&' only if not part of a legal entity sequence */
-	if (*s == '&' && !(xml_isentity(s))) {
-	    sub = "&amp;";
-	    len = 5;
-	}
-	/* '<' '>' are safe to substitute even if string is already UTF-8 coded
-	 * since UTF-8 strings won't contain '<' or '>' */
-	else if (*s == '<') {
-	    sub = "&lt;";
-	    len = 4;
-	}
-	else if (*s == '>') {
-	    sub = "&gt;";
-	    len = 4;
-	}
-	else if (*s == '-') {	/* can't be used in xml comment strings */
-	    sub = "&#45;";
-	    len = 5;
-	}
-	else if (*s == ' ' && prev && *prev == ' ') {
-	    /* substitute 2nd and subsequent spaces with required_spaces */
-	    sub = "&#160;";  /* inkscape doesn't recognise &nbsp; */
-	    len = 6;
-	}
-	else if (*s == '"') {
-	    sub = "&quot;";
-	    len = 6;
-	}
-	else if (*s == '\'') {
-	    sub = "&#39;";
-	    len = 5;
-	}
-	else {
-	    sub = s;
-	    len = 1;
-	}
-	while (len--) {
-	    *p++ = *sub++;
-	    pos++;
-	}
-	prev = s;
-	s++;
-    }
-    *p = '\0';
-    return buf;
-}
 
 int l_int(void *obj, Agsym_t * attr, int def)
 {

cmd/smyrna/smyrna_utils.h
@@ -13,7 +13,6 @@
 #include "smyrnadefs.h"
 #include <cgraph/cgraph.h>
 
-extern char *xml_string(char *s);
 extern int l_int(void *obj, Agsym_t * attr, int def);
 extern float l_float(void *obj, Agsym_t * attr, float def);
 extern int getAttrBool(Agraph_t* g,void* obj,char* attr_name,int def);

commit e0c17caa8fbf1f6275dc7f191b0964fc9d4c56e2

smyrna EmbedText: fix free on non-heap pointer
When opening an xdot source with Smyrna, Address Sanitizer observes:

  $ echo 'graph { a -- b; }' | dot -Txdot -o test.dot
  $ smyrna
  $ # File → Open, open test.dot
  =================================================================
  ==81239==ERROR: AddressSanitizer: attempting free on address which was not malloc()-ed: 0x60200031fa11 in thread T0
      #0 0x7ffff74b4537 in __interceptor_free ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:127
      #1 0x555555b2e53a in EmbedText cmd/smyrna/draw.c:343
      #2 0x555555b58ff1 in draw_xdot cmd/smyrna/topviewfuncs.c:87
      #3 0x555555b5c2f6 in renderNodes cmd/smyrna/topviewfuncs.c:303
      #4 0x555555b628af in updateSmGraph cmd/smyrna/topviewfuncs.c:816
      #5 0x555555b63023 in initSmGraph cmd/smyrna/topviewfuncs.c:840
      #6 0x555555b718ef in refreshViewport cmd/smyrna/viewport.c:418
      #7 0x555555b71a23 in activate cmd/smyrna/viewport.c:426
      #8 0x555555b721d1 in add_graph_to_viewport cmd/smyrna/viewport.c:440
      #9 0x555555b708a9 in add_graph_to_viewport_from_file cmd/smyrna/viewport.c:366
      #10 0x555555b884b2 in mOpenSlot gui/menucallbacks.c:56
      #11 0x7ffff6903d2e in g_closure_invoke (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x14d2e)
      #12 0x7ffff691fc35  (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x30c35)
      #13 0x7ffff6921613 in g_signal_emit_valist (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x32613)
      #14 0x7ffff6921862 in g_signal_emit (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x32862)
      #15 0x7ffff706b41b in gtk_widget_activate (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x26b41b)
      #16 0x7ffff6f55344 in gtk_menu_shell_activate_item (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x155344)
      #17 0x7ffff6f56ed2  (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x156ed2)
      #18 0x7ffff6f434e6  (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x1434e6)
      #19 0x7ffff6903d2e in g_closure_invoke (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x14d2e)
      #20 0x7ffff691f623  (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x30623)
      #21 0x7ffff6921025 in g_signal_emit_valist (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x32025)
      #22 0x7ffff6921862 in g_signal_emit (/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x32862)
      #23 0x7ffff706f023  (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x26f023)
      #24 0x7ffff6f410a3 in gtk_propagate_event (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x1410a3)
      #25 0x7ffff6f426ea in gtk_main_do_event (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x1426ea)
      #26 0x7ffff6da617a  (/lib/x86_64-linux-gnu/libgdk-x11-2.0.so.0+0x6717a)
      #27 0x7ffff63aad3a in g_main_context_dispatch (/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x55d3a)
      #28 0x7ffff64002b7  (/lib/x86_64-linux-gnu/libglib-2.0.so.0+0xab2b7)
      #29 0x7ffff63aa2b2 in g_main_loop_run (/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x552b2)
      #30 0x7ffff6f402e1 in gtk_main (/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0+0x1402e1)
      #31 0x555555b44917 in windowedMode cmd/smyrna/main.c:160
      #32 0x555555b4514d in main cmd/smyrna/main.c:235
      #33 0x7ffff5829d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58
      #34 0x7ffff5829e3f in __libc_start_main_impl ../csu/libc-start.c:392
      #35 0x555555b1c604 in _start (/tmp/tmp.xTLIqFGuru/bin/smyrna+0x5c8604)

What is occurring here is that the pointer to the original allocated memory in
`EmbedText`, `escaped`, is advanced by its callback, `put`. The effect of this
is that `escaped` no longer points to the start of the allocation when the
`free` call is arrived at.

This bug was introduced in 2d10d171bbeccf1c57370c86f051f4805129de9c.

cmd/smyrna/draw.c
@@ -333,8 +333,8 @@ static void EmbedText(xdot_op *op, int param) {
 
 		// XML-escape the text
 		const xml_flags_t flags = {.dash = 1, .nbsp = 1};
-		char **ptr = &escaped;
-		(void)gv_xml_escape(o->op.u.text.text, flags, put, ptr);
+		char *ptr = escaped;
+		(void)gv_xml_escape(o->op.u.text.text, flags, put, &ptr);
 
 		o->font = glNewFont(view->widgets, escaped, &view->penColor,
 		                    font_op->op.u.font.name, font_op->op.u.font.size,