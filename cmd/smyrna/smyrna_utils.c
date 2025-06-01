cdc4d98e7c66abb2b39d7120711cb6a8e338e9e7
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-06-01 08:59:44 -0700

    smyrna getAttrBool: condense 'attr' initialization

--- cmd/smyrna/smyrna_utils.c
+++ cmd/smyrna/smyrna_utils.c
@@ -32,8 +32,7 @@ float l_float(void *obj, Agsym_t * attr, float def)
 }
 int getAttrBool(Agraph_t* g,void* obj,char* attr_name,int def)
 {
-    Agsym_t* attr;
-    attr = agattr_text(g, AGTYPE(obj), attr_name,0);
+    Agsym_t *const attr = agattr_text(g, AGTYPE(obj), attr_name, 0);
     return late_bool(obj, attr,def);
 }
 int getAttrInt(Agraph_t* g,void* obj,char* attr_name,int def)
