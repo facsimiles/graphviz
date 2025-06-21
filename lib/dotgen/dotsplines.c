facsimiles/graphviz

commit 9d3c9e1d04cb270f8f2f721a658c4e66dde8c9d7

dotgen dot_splines_: bail out if 'make_flat_edge' fails
Continuing does not make sense if we have failed to create flat edges.

Gitlab: #1581

lib/dotgen/dotsplines.c
@@ -428,7 +428,10 @@ static int dot_splines_(graph_t *g, int normalize) {
           updateBB(g, ED_label(e));
       }
     } else if (ND_rank(agtail(e0)) == ND_rank(aghead(e0))) {
-      make_flat_edge(g, &sd, &P, edges, ind, cnt, et);
+      const int rc = make_flat_edge(g, &sd, &P, edges, ind, cnt, et);
+      if (rc != 0) {
+        return rc;
+      }
     } else
       make_regular_edge(g, &sd, &P, edges, ind, cnt, et);
   }

commit e84a88abd47bf58dbce0ae7b10243972381a3c56

dotgen dot_splines_: fix memory leak of 'sd.Rank_box'
When running test_1453, Address Sanitizer notices:

  Direct leak of 160 byte(s) in 1 object(s) allocated from:
      #0 0x7ffff74b4a57 in __interceptor_calloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:154
      #1 0x7ffff169b074 in gv_calloc ../../lib/util/alloc.h:36
      #2 0x7ffff16a8596 in dot_splines_ lib/dotgen/dotsplines.c:349
      #3 0x7ffff16af1f4 in dot_splines lib/dotgen/dotsplines.c:495
      #4 0x7ffff161ce6b in dotLayout lib/dotgen/dotinit.c:332
      #5 0x7ffff162042a in doDot lib/dotgen/dotinit.c:454
      #6 0x7ffff1620c53 in dot_layout lib/dotgen/dotinit.c:518
      #7 0x7ffff6a1ecd4  (/tmp/tmp.t8AoJBXqk5/lib+0x41ecd4)
      #8 0x5555555555df in main cmd/dot/dot.c:72
      #9 0x7ffff5829d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58

This bug was introduced in 9d3c9e1d04cb270f8f2f721a658c4e66dde8c9d7.

Reported-by: GCC 11.4.0 -fsanitize=address

lib/dotgen/dotsplines.c
@@ -427,6 +427,7 @@ static int dot_splines_(graph_t *g, int normalize) {
     } else if (ND_rank(agtail(e0)) == ND_rank(aghead(e0))) {
       const int rc = make_flat_edge(g, sd, &P, edges, ind, cnt, et);
       if (rc != 0) {
+        free(sd.Rank_box);
         return rc;
       }
     } else