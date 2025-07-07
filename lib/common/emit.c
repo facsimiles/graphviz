facsimiles/graphviz

commit 180f77a87f216e9b60b3eb1d5acce11cb5d6c034

remove unnecessary 'Show_boxes' cast
`Show_boxes` is already a `const char**`.

lib/common/emit.c
@@ -4103,7 +4103,7 @@ int gvRenderJobs (GVC_t * gvc, graph_t * g)
     		/* Show_boxes is not defined, if at all, 
                  * until splines are generated in dot 
                  */
-	    job->common->show_boxes = (const char**)Show_boxes; 
+	    job->common->show_boxes = Show_boxes;
 #endif
 	    emit_graph(job, g);
 	}

commit 5497cea2bf75ed27ff727237c13da9344b677cc2

common gvRenderJobs: repair debug compilation under GCC 14
GCC 14 gets more strict and turns some things that were previously warnings into
errors:

  emit.c: In function 'gvRenderJobs':
  emit.c:3973:35: error: assignment to 'const char **' from incompatible pointer
    type 'char **' [-Wincompatible-pointer-types]
   3973            job->common->show_boxes = show_boxes_front(&Show_boxes);

This change effectively reverts 180f77a87f216e9b60b3eb1d5acce11cb5d6c034 which
introduced this issue. I am not sure why
180f77a87f216e9b60b3eb1d5acce11cb5d6c034’s commit message says `Show_boxes` was
a `const char **`. It was not; it was a `char **`.

A cast is not a particularly clean way of solving this and we should just
realign these types at the next opportunity for an API break.

Gitlab: #2699
Reported-by: Steve Roush
Reported-by: GCC 14.2.0 -Wincompatible-pointer-types

lib/common/emit.c
@@ -3973,7 +3973,16 @@ int gvRenderJobs (GVC_t * gvc, graph_t * g)
                  */
 	    show_boxes_append(&Show_boxes, NULL);
 	    show_boxes_sync(&Show_boxes);
-	    job->common->show_boxes = show_boxes_front(&Show_boxes);
+// FIXME: remove the cast and change `show_boxes` to a `char **` at the next API
+// break
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#endif
+	    job->common->show_boxes = (const char **)show_boxes_front(&Show_boxes);
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 #endif
 	    emit_graph(job, g);
 	}