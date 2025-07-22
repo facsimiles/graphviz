a4a00330945977bdbe0a4f1ff801c7f359d37073
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-07-22 17:01:05 +1000

    gvpr main: remove 'DEBUG' alternative
    This has proven confusing to users. A separate path that does significantly
    different things including running the user’s program twice does not seem
    desirable in contemporary environments.
    
    Gitlab: closes #2711
    Reported-by: Steve Roush

--- cmd/gvpr/gvprmain.c
+++ cmd/gvpr/gvprmain.c
@@ -15,78 +15,9 @@
  * Written by Emden Gansner
  */
 
-
-#include "config.h"
-#include <limits.h>
-#include <stddef.h>
-#include <stdio.h>
-
-#include <cgraph/cgraph.h>
 #include <gvpr/gvpr.h>
 #include <util/exit.h>
 
-#ifdef DEBUG
-static ssize_t outfn (void* sp, const char *buf, size_t nbyte, void* dp)
-{
-  if (nbyte > INT_MAX) {
-    return -1;
-  }
-  return printf("<stdout>%.*s", (int)nbyte, buf);
-}
-
-static ssize_t errfn (void* sp, const char *buf, size_t nbyte, void* dp)
-{
-  if (nbyte > INT_MAX) {
-    return -1;
-  }
-  return fprintf(stderr, "<stderr>%.*s", (int)nbyte, buf);
-}
-
-static int iofread(void *chan, char *buf, int bufsize)
-{
-  return (int)fread(buf, 1, (size_t)bufsize, chan);
-}
-
-static int ioputstr(void *chan, const char *str)
-{
-  return fputs(str, chan);
-}
-
-static int ioflush(void *chan)
-{
-  return fflush(chan);
-}
-
-static Agiodisc_t gprIoDisc = { iofread, ioputstr, ioflush };
-
-static Agdisc_t gprDisc = { &AgIdDisc, &gprIoDisc };
-
-int
-main (int argc, char* argv[])
-{
-    Agraph_t* gs[2];
-    Agraph_t* g = agread(stdin, &gprDisc);
-    int rv;
-    gvpropts opts;
-
-    gs[0] = g;
-    gs[1] = 0;
-    opts.ingraphs = gs;
-    opts.out = outfn;
-    opts.err = errfn;
-    opts.flags = GV_USE_OUTGRAPH;
-    opts.bindings = 0;
-    
-    rv = gvpr (argc, argv, &opts);
-
-    fprintf(stderr, "rv %d\n", rv);
-
-    rv = gvpr (argc, argv, &opts);
-
-    graphviz_exit(rv);
-}
-
-#else
 int
 main (int argc, char* argv[])
 {
@@ -99,8 +30,6 @@ main (int argc, char* argv[])
     
     graphviz_exit(gvpr(argc, argv, &opts));
 }
-
-#endif
 
 /**
  * @dir cmd/gvpr
