24c8a2cd2b00014cdfc6e560c2d603404363b37f
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-08-02 21:10:40 +1000

    CI: run 'pip install' steps on Windows from within windows_build.py
    Following 78731ccc7bcdb4f28da658e20ec467bb676bab16, this moves another step of
    the Windows CI process into Python to more robustly handle failures. It looks a
    bit strange installing Python dependencies from within a Python script, but
    nothing in this script itself uses any dependencies beyond the Python standard
    library so this works fine.

--- ci/windows_build.py
+++ ci/windows_build.py
@@ -90,6 +90,13 @@ def main(args: list[str]) -> int:
     # find the repository root directory
     root = Path(__file__).resolve().parent.parent
 
+    # install Python dependencies
+    run(
+        [sys.executable, "-m", "pip", "install", "--requirement", "requirements.txt"],
+        root,
+        None,
+    )
+
     # retrieve submodules, dependencies are stored there
     run(["git", "submodule", "update", "--init", "--depth=1"], root, None)
 
