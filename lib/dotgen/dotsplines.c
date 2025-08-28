facsimiles/graphviz

commit 9d3c9e1d04cb270f8f2f721a658c4e66dde8c9d7

dotgen dot_splines_: bail out if 'make_flat_edge' fails
Continuing does not make sense if we have failed to create flat edges.

Gitlab: #1581

lib/dotgen/dotsplines.c
@@ -428,7 +428,10 @@ static int dot_splines_(graph_t *g, int normalize) {
           updateBB(g, ED_label(e));
       }
     } else if (ND_rank(agtail(e0)) == ND_rank(aghead(e0))) {
-      make_flat_edge(g, &sd, &P, edges, ind, cnt, et);
+      const int rc = make_flat_edge(g, &sd, &P, edges, ind, cnt, et);
+      if (rc != 0) {
+        return rc;
+      }
     } else
       make_regular_edge(g, &sd, &P, edges, ind, cnt, et);
   }

commit 38ae8559e5f895ad6538675c62f490a2101f33c5

dotgen dot_splines_: fix memory leak of 'edges'
This bug was introduced in 9d3c9e1d04cb270f8f2f721a658c4e66dde8c9d7.

lib/dotgen/dotsplines.c
@@ -417,6 +417,7 @@ static int dot_splines_(graph_t *g, int normalize) {
           make_flat_edge(g, sd, &P, LIST_FRONT(&edges), ind, cnt, et);
       if (rc != 0) {
         free(sd.Rank_box);
+        LIST_FREE(&edges);
         return rc;
       }
     } else