facsimiles/graphviz

commit 101abbda84e49daddb44b6d788159a60b0747463

gvpr: store arguments as string views instead of dynamically allocated strings
The lifetime of the backing memory for these arguments is fully contained, so
there is no need to allocate and copy this data.

lib/gvpr/compile.c
@@ -33,6 +33,7 @@
 #include <util/list.h>
 #include <util/prisize_t.h>
 #include <util/startswith.h>
+#include <util/strview.h>
 #include <util/unreachable.h>
 
 static int isedge(Agobj_t *obj) {
@@ -462,13 +463,18 @@ static int lookup(Expr_t *pgm, Agobj_t *objp, Exid_t *sym, Extype_t *v) {
   return 0;
 }
 
-// return value associated with $n
-static char *getArg(int n, Gpr_t *state) {
+/// get value associated with $n
+///
+/// @param out [out] Found argument on success
+/// @return 0 on success
+static int getArg(int n, Gpr_t *state, strview_t *out) {
+  assert(out != NULL);
   if (n >= state->argc) {
     exerror("program references ARGV[%d] - undefined", n);
-    return 0;
+    return -1;
   }
-  return state->argv[n];
+  *out = state->argv[n];
+  return 0;
 }
 
 static int setDfltAttr(Agraph_t *gp, char *k, char *name, char *value) {
@@ -1415,9 +1421,17 @@ static Extype_t getval(Expr_t *pgm, Exnode_t *node, Exid_t *sym, Exref_t *ref,
     args = env;
     state = disc->user;
     switch (sym->index) {
-    case A_ARGV:
-      v.string = getArg(args[0].integer, state);
+    case A_ARGV: {
+      strview_t arg;
+      if (getArg(args[0].integer, state, &arg) != 0) {
+        break;
+      }
+      v.string = exstralloc(pgm, arg.size + 1);
+      if (arg.size > 0) {
+        memcpy(v.string, arg.data, arg.size);
+      }
       break;
+    }
     default:
       exerror("unknown array name: %s", sym->name);
       v.string = 0;

lib/gvpr/gprstate.h
@@ -23,6 +23,7 @@ extern "C" {
 #include <stdio.h>
 #include <expr/expr.h>
 #include <util/list.h>
+#include <util/strview.h>
 #include "gvpr.h"
 
     typedef enum { TV_flat, TV_ne, TV_en, 
@@ -50,7 +51,7 @@ extern "C" {
 	Agedge_t *tvedge;
 	int name_used;
 	int argc;
-	char **argv;
+	strview_t *argv;
 	int flags;
 	gvprbinding* bindings;
 	size_t n_bindings;
@@ -60,7 +61,7 @@ extern "C" {
     typedef struct {
 	FILE *outFile;
 	int argc;
-	char **argv;
+	strview_t *argv;
 	Exerror_f errf;
 	Exexit_f exitf;
 	int flags;

lib/gvpr/gvpr.c
@@ -38,6 +38,7 @@
 #include <util/gv_find_me.h>
 #include <util/list.h>
 #include <util/path.h>
+#include <util/strview.h>
 #include <util/unreachable.h>
 
 static char *Info[] = {
@@ -69,7 +70,7 @@ typedef struct {
   int readAhead;
   char **inFiles;
   int argc;
-  char **argv;
+  strview_t *argv;
   int state; /* > 0 : continue; <= 0 finish */
   int verbose;
 } options;
@@ -142,11 +143,10 @@ static char *gettok(char **sp) {
  * argc is the current number of arguments, with the arguments
  * stored in *argv.
  */
-static int parseArgs(char *s, int argc, char ***argv) {
+static int parseArgs(char *s, int argc, strview_t **argv) {
   int i, cnt = 0;
   char *args[NUM_ARGS];
   char *t;
-  char **av;
 
   assert(argc >= 0);
 
@@ -163,9 +163,10 @@ static int parseArgs(char *s, int argc, char ***argv) {
   if (cnt) {
     int oldcnt = argc;
     argc = oldcnt + cnt;
-    av = gv_recalloc(*argv, (size_t)oldcnt, (size_t)argc, sizeof(char *));
+    strview_t *const av =
+        gv_recalloc(*argv, (size_t)oldcnt, (size_t)argc, sizeof(strview_t));
     for (i = 0; i < cnt; i++)
-      av[oldcnt + i] = gv_strdup(args[i]);
+      av[oldcnt + i] = strview(args[i], '\0');
     *argv = av;
   }
   return argc;
@@ -399,8 +400,6 @@ static void freeOpts(options opts) {
   free(opts.inFiles);
   if (opts.useFile)
     free(opts.program);
-  for (int i = 0; i < opts.argc; i++)
-    free(opts.argv[i]);
   free(opts.argv);
 }
 

commit 17bb5f932880e31fe015ad5c28f18d4d04258e5c

gvpr: fix -Wsometimes-uninitialized warning on macOS
101abbda84e49daddb44b6d788159a60b0747463 introduced a new compilation warning
that is failing the macOS CMake build in CI:

    /Users/gitlab/builds/graphviz/graphviz/lib/gvpr/compile.c:1426:11: error:
      variable 'v' is used uninitialized whenever 'if' condition is true
      [-Werror,-Wsometimes-uninitialized]
          if (getArg(args[0].integer, state, &arg) != 0) {
              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/gitlab/builds/graphviz/graphviz/lib/gvpr/compile.c:1439:12: note:
      uninitialized use occurs here
        return v;
               ^
    /Users/gitlab/builds/graphviz/graphviz/lib/gvpr/compile.c:1426:7: note:
      remove the 'if' if its condition is always false
          if (getArg(args[0].integer, state, &arg) != 0) {
          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/gitlab/builds/graphviz/graphviz/lib/gvpr/compile.c:565:3: note:
      variable 'v' is declared here
      Extype_t v;
      ^

lib/gvpr/compile.c
@@ -1424,7 +1424,7 @@ static Extype_t getval(Expr_t *pgm, Exnode_t *node, Exid_t *sym, Exref_t *ref,
     case A_ARGV: {
       strview_t arg;
       if (getArg(args[0].integer, state, &arg) != 0) {
-        break;
+        return (Extype_t){0};
       }
       v.string = exstralloc(pgm, arg.size + 1);
       if (arg.size > 0) {