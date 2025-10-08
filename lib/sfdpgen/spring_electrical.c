b51f9b7711f0f5b0a62fc5e9300c14baef504a2a
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-10-07 17:06:11 -0700

    sfdpgen shorting_edge_label_nodes: remove manual management of 'irn', 'jcn'
    CodeChecker says:
    
      [HIGH] lib/sfdpgen/spring_electrical.c:1057:10: Array access (from variable
        'irn') results in a null pointer dereference [core.NullDereference]
        irn[nz] = mask[i];
                ^
    
      [HIGH] lib/sfdpgen/spring_electrical.c:1064:14: Array access (from variable
        'irn') results in a null pointer dereference [core.NullDereference]
            irn[nz] = mask[i];
                    ^
    
    These both appear to be false positives. But determining this requires carefully
    examining the two loops in this function. We can avoid this complexity for both
    humans and automated tools (as well as avoiding the cost of repeated loops) by
    using generic lists that expand on-demand.
    
    Note that the two `LIST_SYNC` calls here are no-ops; both lists should already
    be synchronized. These are included for robustness in case the list
    implementation changes in future.
    
    Reported-by: CodeChecker 6.26.2

--- lib/sfdpgen/spring_electrical.c
+++ lib/sfdpgen/spring_electrical.c
@@ -9,6 +9,7 @@
  *************************************************************************/
 
 #include "config.h"
+#include <assert.h>
 #include <sparse/SparseMatrix.h>
 #include <sfdpgen/spring_electrical.h>
 #include <sparse/QuadTree.h>
@@ -17,6 +18,7 @@
 #include <neatogen/overlap.h>
 #include <common/types.h>
 #include <common/arith.h>
+#include <limits.h>
 #include <math.h>
 #include <common/globals.h>
 #include <stdbool.h>
@@ -1013,8 +1015,8 @@ static void attach_edge_label_coordinates(int dim, SparseMatrix A, int n_edge_la
 }
 
 static SparseMatrix shorting_edge_label_nodes(SparseMatrix A, int n_edge_label_nodes, int *edge_label_nodes){
-  int i, id = 0, nz, j, jj, ii;
-  int *ia = A->ia, *ja = A->ja, *irn = NULL, *jcn = NULL;
+  int i, id = 0, j, jj, ii;
+  int *ia = A->ia, *ja = A->ja;
   SparseMatrix B;
 
   int *mask = gv_calloc(A->m, sizeof(int));
@@ -1029,49 +1031,36 @@ static SparseMatrix shorting_edge_label_nodes(SparseMatrix A, int n_edge_label_n
     if (mask[i] > 0) mask[i] = id++;
   }
 
-  nz  = 0;
+  LIST(int) irn = {0};
+  LIST(int) jcn = {0};
   for (i = 0; i < A->m; i++){
     if (mask[i] < 0) continue;
     for (j = ia[i]; j < ia[i+1]; j++){
       if (mask[ja[j]] >= 0) {
-	nz++;
-	continue;
-      }
-      ii = ja[j];
-      for (jj = ia[ii]; jj < ia[ii+1]; jj++){
-	if (ja[jj] != i && mask[ja[jj]] >= 0) nz++;
-      }
-    }
-  }
-
-  if (nz > 0) {
-    irn = gv_calloc(nz, sizeof(int));
-    jcn = gv_calloc(nz, sizeof(int));
-  }
-
-  nz = 0;
-  for (i = 0; i < A->m; i++){
-    if (mask[i] < 0) continue;
-    for (j = ia[i]; j < ia[i+1]; j++){
-      if (mask[ja[j]] >= 0) {
-	irn[nz] = mask[i];
-	jcn[nz++] = mask[ja[j]];
+	LIST_APPEND(&irn, mask[i]);
+	LIST_APPEND(&jcn, mask[ja[j]]);
 	continue;
       }
       ii = ja[j];
       for (jj = ia[ii]; jj < ia[ii+1]; jj++){
 	if (ja[jj] != i && mask[ja[jj]] >= 0) {
-	    irn[nz] = mask[i];
-	    jcn[nz++] = mask[ja[jj]];
+	    LIST_APPEND(&irn, mask[i]);
+	    LIST_APPEND(&jcn, mask[ja[jj]]);
 	}
       }
     }
   }
 
-  B = SparseMatrix_from_coordinate_arrays(nz, id, id, irn, jcn, NULL, MATRIX_TYPE_PATTERN, sizeof(double));
+  LIST_SYNC(&irn);
+  LIST_SYNC(&jcn);
+  assert(LIST_SIZE(&irn) <= INT_MAX);
+  B = SparseMatrix_from_coordinate_arrays((int)LIST_SIZE(&irn), id, id,
+                                          LIST_FRONT(&irn), LIST_FRONT(&jcn),
+                                          NULL, MATRIX_TYPE_PATTERN,
+                                          sizeof(double));
 
-  free(irn);
-  free(jcn);
+  LIST_FREE(&irn);
+  LIST_FREE(&jcn);
   free(mask);
   return B;
 
