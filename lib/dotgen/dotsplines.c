facsimiles/graphviz

commit 9d3c9e1d04cb270f8f2f721a658c4e66dde8c9d7

dotgen dot_splines_: bail out if 'make_flat_edge' fails
Continuing does not make sense if we have failed to create flat edges.

Gitlab: #1581

lib/dotgen/dotsplines.c
@@ -428,7 +428,10 @@ static int dot_splines_(graph_t *g, int normalize) {
           updateBB(g, ED_label(e));
       }
     } else if (ND_rank(agtail(e0)) == ND_rank(aghead(e0))) {
-      make_flat_edge(g, &sd, &P, edges, ind, cnt, et);
+      const int rc = make_flat_edge(g, &sd, &P, edges, ind, cnt, et);
+      if (rc != 0) {
+        return rc;
+      }
     } else
       make_regular_edge(g, &sd, &P, edges, ind, cnt, et);
   }

commit f29777f83bd4b0ded8d22d47049fa21f75ee7f9f

dotgen dot_splines_: fix memory leak of 'P.boxes'
CodeChecker says:

  [MEDIUM] lib/dotgen/dotsplines.c:419:9: Potential leak of memory pointed to by
    'P.boxes' [unix.Malloc]
          free(sd.Rank_box);
          ^

e84a88abd47bf58dbce0ae7b10243972381a3c56 fixed one problem here but did not
cover this additional leak. This bug was introduced by
9d3c9e1d04cb270f8f2f721a658c4e66dde8c9d7.

Reported-by: CodeChecker 6.26.2

lib/dotgen/dotsplines.c
@@ -418,6 +418,7 @@ static int dot_splines_(graph_t *g, int normalize) {
       if (rc != 0) {
         free(sd.Rank_box);
         LIST_FREE(&edges);
+        free(P.boxes);
         return rc;
       }
     } else