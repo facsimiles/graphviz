818b42ee9e300281e0f2ec284496fff03d53cf17
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-10-20 17:07:43 -0700

    gvpr: remove 'gv' prefix from timer functions
    Now that these functions are not exposed externally, there is no need to
    namespace them.

--- lib/gvpr/gvpr.c
+++ lib/gvpr/gvpr.c
@@ -82,9 +82,9 @@ typedef clock_t mytime_t;
 
 static mytime_t T;
 
-static void gvstart_timer(void) { GET_TIME(T); }
+static void start_timer(void) { GET_TIME(T); }
 
-static double gvelapsed_sec(void) {
+static double elapsed_sec(void) {
   mytime_t end;
   double rv;
 
@@ -933,7 +933,7 @@ static int gvpr_core(int argc, char *argv[], gvpropts *uopts,
   }
 
   if (gs->opts.verbose)
-    gvstart_timer();
+    start_timer();
   gs->prog = parseProg(gs->opts.program, gs->opts.useFile);
   if (gs->prog == NULL) {
     return 1;
@@ -974,7 +974,7 @@ static int gvpr_core(int argc, char *argv[], gvpropts *uopts,
   bool incoreGraphs = uopts->ingraphs;
 
   if (gs->opts.verbose)
-    fprintf(stderr, "Parse/compile/init: %.2f secs.\n", gvelapsed_sec());
+    fprintf(stderr, "Parse/compile/init: %.2f secs.\n", elapsed_sec());
   /* do begin */
   if (gs->xprog->begin_stmt != NULL)
     exeval(gs->xprog->prog, gs->xprog->begin_stmt, gs->state);
@@ -987,12 +987,12 @@ static int gvpr_core(int argc, char *argv[], gvpropts *uopts,
       gs->ing = newIng(0, gs->opts.inFiles, ing_read);
 
     if (gs->opts.verbose)
-      gvstart_timer();
+      start_timer();
     Agraph_t *nextg = NULL;
     for (gs->state->curgraph = nextGraph(gs->ing); gs->state->curgraph;
          gs->state->curgraph = nextg) {
       if (gs->opts.verbose)
-        fprintf(stderr, "Read graph: %.2f secs.\n", gvelapsed_sec());
+        fprintf(stderr, "Read graph: %.2f secs.\n", elapsed_sec());
       gs->state->infname = fileName(gs->ing);
       if (gs->opts.readAhead)
         nextg = gs->state->nextgraph = nextGraph(gs->ing);
@@ -1021,7 +1021,7 @@ static int gvpr_core(int argc, char *argv[], gvpropts *uopts,
       if (gs->xprog->endg_stmt != NULL)
         exeval(gs->xprog->prog, gs->xprog->endg_stmt, gs->state);
       if (gs->opts.verbose)
-        fprintf(stderr, "Finish graph: %.2f secs.\n", gvelapsed_sec());
+        fprintf(stderr, "Finish graph: %.2f secs.\n", elapsed_sec());
 
       /* if $O == $G and $T is empty, delete $T */
       if (gs->state->outgraph == gs->state->curgraph &&
@@ -1046,11 +1046,11 @@ static int gvpr_core(int argc, char *argv[], gvpropts *uopts,
       gs->state->outgraph = 0;
 
       if (gs->opts.verbose)
-        gvstart_timer();
+        start_timer();
       if (!gs->opts.readAhead)
         nextg = nextGraph(gs->ing);
       if (gs->opts.verbose && nextg != NULL) {
-        fprintf(stderr, "Read graph: %.2f secs.\n", gvelapsed_sec());
+        fprintf(stderr, "Read graph: %.2f secs.\n", elapsed_sec());
       }
     }
   }
