57dfffc2ea271d7b0e46d6e89707044423bc1035
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-10-24 17:38:06 -0700

    cdt: clang-format dtwalk.c

--- lib/cdt/dtwalk.c
+++ lib/cdt/dtwalk.c
@@ -1,4 +1,4 @@
-#include	<cdt/dthdr.h>
+#include <cdt/dthdr.h>
 
 /*	Walk a dictionary and all dictionaries viewed through it.
 **	userf:	user function
@@ -6,15 +6,14 @@
 **	Written by Kiem-Phong Vo (5/25/96)
 */
 
-int dtwalk(Dt_t *dt, int (*userf)(void*, void*), void *data) {
-	int		rv;
+int dtwalk(Dt_t *dt, int (*userf)(void *, void *), void *data) {
+  int rv;
 
-	for (void *obj = dtfirst(dt); obj; )
-	{
-		void *const next = dtnext(dt,obj);
-		if ((rv = userf(obj, data)) < 0)
-			return rv;
-		obj = next;
-	}
-	return 0;
+  for (void *obj = dtfirst(dt); obj;) {
+    void *const next = dtnext(dt, obj);
+    if ((rv = userf(obj, data)) < 0)
+      return rv;
+    obj = next;
+  }
+  return 0;
 }
