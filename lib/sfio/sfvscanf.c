dab695e7fc926d3d007cb4ab32a160d186af9bc2
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-06 17:01:19 -0800

    sfio sfvscanf: remove dead store to 't_str'
    CodeChecker says:
    
      [LOW] lib/sfio/sfvscanf.c:170:4: Value stored to 't_str' is never read
        [deadcode.DeadStores]
            t_str = _Sffmtintf(t_str + 1, &n);
            ^
    
    It seems to have gone unnoticed that 515db14ba4e311145741e1675848131226150a08
    made the results (both the returned `t_str` and written `n` values) of this call
    unused.
    
    Gitlab: #1998
    Reported-by: CodeChecker 6.26.2

--- lib/sfio/sfvscanf.c
+++ lib/sfio/sfvscanf.c
@@ -167,7 +167,6 @@ int sfvscanf(FILE *f, Sffmt_t *ft) {
 		    if (*t_str != '*')
 			n_str = (form - 1) - t_str;
 		    else {
-			t_str = _Sffmtintf(t_str + 1, &n);
 			++argn;
 
 			FMTSET(ft, form, LEFTP, 0, 0, 0, 0, 0, NULL, 0);
