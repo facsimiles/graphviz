f5e8303012b0f497bcfdf3c0392f46af5f2bd435
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-17 17:29:32 -0800

    neatogen: lift 'right' into a first class function

--- lib/neatogen/dijkstra.c
+++ lib/neatogen/dijkstra.c
@@ -38,7 +38,8 @@ typedef DistType Word;
 
 static int left(int i) { return 2 * i; }
 
-#define right(i) (2*(i)+1)
+static int right(int i) { return 2 * i + 1; }
+
 #define parent(i) ((i)/2)
 #define insideHeap(h,i) ((i)<h->heapSize)
 #define greaterPriority(h,i,j,dist) (dist[h->data[i]]<dist[h->data[j]])
