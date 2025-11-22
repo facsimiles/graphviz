14437e7120c27dfda28916b7b5f0eed5812e64d5
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-11-21 17:03:34 -0800

    common edge_in_layer: make string comparisons clearer

--- lib/common/emit.c
+++ lib/common/emit.c
@@ -1605,11 +1605,11 @@ static bool edge_in_layer(GVJ_t *job, edge_t * e)
     const char *const pe = late_string(e, E_layer, "");
     if (selectedlayer(job, pe))
 	return true;
-    if (pe[0])
+    if (!streq(pe, ""))
 	return false;
     for (int cnt = 0; cnt < 2; cnt++) {
 	const char *const pn = late_string(cnt < 1 ? agtail(e) : aghead(e), N_layer, "");
-	if (pn[0] == '\0' || selectedlayer(job, pn))
+	if (streq(pn, "") || selectedlayer(job, pn))
 	    return true;
     }
     return false;
