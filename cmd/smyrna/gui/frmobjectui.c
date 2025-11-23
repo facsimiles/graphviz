facsimiles/graphviz

commit 21c12a59b10e5092c8ed8d62f515a689af97337e

smyrna create_filtered_list: replace manual binary search with stdlib

cmd/smyrna/gui/frmobjectui.c
@@ -216,34 +216,21 @@ static attr_t *binarySearch(attr_list *l, const char *searchKey) {
   return NULL;
 }
 
-static attr_t *pBinarySearch(attr_list *l, const char *searchKey) {
-    size_t low = 0;
-    size_t high = attrs_size(&l->attributes) - 1;
-
-    while (high != SIZE_MAX && low <= high) {
-	size_t middle = (low + high) / 2;
-	int res = strncasecmp(searchKey, attrs_get(&l->attributes, middle)->name, strlen(searchKey));
-	if (res == 0) {
-	    return attrs_get(&l->attributes, middle);
-	}
-	else if (res < 0) {
-	    high = middle - 1;
-	} else {
-	    low = middle + 1;
-	}
-    }
-    return NULL;
+static int cmp(const void *key, const void *candidate) {
+  const attr_t *const a = candidate;
+  return strncasecmp(key, a->name, strlen(key));
 }
 
 static void create_filtered_list(const char *prefix, attr_list *sl,
                                  attr_list *tl) {
-    attr_t *at;
     int objKind = get_object_type();
 
     if (strlen(prefix) == 0)
 	return;
     /*locate first occurrence */
-    at = pBinarySearch(sl, prefix);
+    attrs_sync(&sl->attributes);
+    attr_t *at = bsearch(prefix, attrs_front(&sl->attributes),
+                         attrs_size(&sl->attributes), sizeof(attr_t), cmp);
     if (!at)
 	return;
 

commit 5a54a8bb32e7c098e0a9de9325848449c3e25d41

smyrna create_filtered_list: fix incorrect pointer indirection with 'bsearch'
This bug was introduced in 21c12a59b10e5092c8ed8d62f515a689af97337e. There is no
changelog entry for this commit because it is considered to be covered by the
entry introduced in the previous commit.

cmd/smyrna/gui/frmobjectui.c
@@ -231,10 +231,11 @@ static void create_filtered_list(const char *prefix, attr_list *sl,
 	return;
     /*locate first occurrence */
     LIST_SYNC(&sl->attributes);
-    attr_t *at = bsearch(prefix, LIST_FRONT(&sl->attributes),
-                         LIST_SIZE(&sl->attributes), sizeof(attr_t), cmp);
-    if (!at)
+    attr_t **attrp = bsearch(prefix, LIST_FRONT(&sl->attributes),
+                             LIST_SIZE(&sl->attributes), sizeof(attr_t *), cmp);
+    if (!attrp)
 	return;
+    attr_t *at = *attrp;
 
     /*go backward to get the first */
     for (int res = 0; at->index > 0 && res == 0; ) {