88b5cf9972fea4dc1f09bab36de1b3adb7d0ef1d
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-01-04 12:26:13 -0800

    common genEllipticPath: reduce scope of 'xB'

--- lib/common/ellipse.c
+++ lib/common/ellipse.c
@@ -203,7 +203,6 @@ static void endPath(bezier_path_t *polypath) {
  * Returned path must be freed by the caller.
  */
 static Ppolyline_t *genEllipticPath(ellipse_t * ep) {
-    double xB;
     double yB;
     double xBDot;
     double yBDot;
@@ -239,7 +238,7 @@ static Ppolyline_t *genEllipticPath(ellipse_t * ep) {
     double bSinEtaB = ep->b * sinEtaB;
     double aSinEtaB = ep->a * sinEtaB;
     double bCosEtaB = ep->b * cosEtaB;
-    xB = ep->cx + aCosEtaB;
+    double xB = ep->cx + aCosEtaB;
     yB = ep->cy + bSinEtaB;
     xBDot = -aSinEtaB;
     yBDot = bCosEtaB;
