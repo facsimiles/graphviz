d83eca7a2f20b6692b260b2a7edc7f448585f839
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-01-17 21:25:37 +1100

    XXX

--- lib/util/dword.c
+++ lib/util/dword.c
@@ -1,22 +1,30 @@
-#include <util/dword.h>
-#include <string.h>
+#include <assert.h>
 #include <stdatomic.h>
+#include <stdbool.h>
+#include <string.h>
+#include <util/dword.h>
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
 
 /// Callers using `dword_t` values are typically implementing lock-free
 /// algorithms. …
-#if defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__) || (defined(__aarch64__) && !defined(__clang__)))
+#if defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__) ||          \
+                          (defined(__aarch64__) && !defined(__clang__)))
 #define FORCE_CAS 1
 #else
 #define FORCE_CAS 0
 #endif
 
-dword_t dword_new(void) {
+dword_t gv_dword_new(void) {
   dword_t d;
   memset(&d, 0, sizeof(d));
   return d;
 }
 
-dword_t dword_atomic_load(_Atomic dword_t *src) {
+dword_t gv_dword_atomic_load(_Atomic dword_t *src) {
+  assert(dst != NULL);
 #if FORCE_CAS
   return __sync_val_compare_and_swap(src, 1, 1);
 #else
@@ -24,25 +32,52 @@ dword_t dword_atomic_load(_Atomic dword_t *src) {
 #endif
 }
 
-void dword_atomic_store(_Atomic dword_t *dst, dword_t src) {
+void gv_dword_atomic_store(_Atomic dword_t *dst, dword_t src) {
+  assert(dst != NULL);
 #if FORCE_CAS
-  {
-    dword_t expected;
-    dword_t old = dword_new();
-    do {
-      expected = old;
-      old = __sync_val_compare_and_swap(dst, expected, src);
-    } while (expected != old);
+  for (dword_t old = gv_dword_new();;) {
+    const dword_t expected = old;
+    old = __sync_val_compare_and_swap(dst, expected, src);
+    if (old != expected) {
+      break;
+    }
   }
 #else
   atomic_store_explicit(dst, src, memory_order_release);
 #endif
 }
 
-bool dword_atomic_cas(_Atomic dword_t *dst, dword_t *expected, dword_t desired) {
+bool gv_dword_atomic_cas(_Atomic dword_t *dst, dword_t *expected,
+                         dword_t desired) {
+  assert(dst != NULL);
+  assert(expected != NULL);
 #if FORCE_CAS
   return __sync_bool_compare_and_swap(dst, expected, desired);
+#elif defined(_WIN64)
+  return _InterlockedCompareExchange128(dst, desired.word[1], desired.word[0],
+                                        expected);
+#elif defined(_WIN32)
+  {
+    const dword_t expectation = *expected;
+    *expected = _InterlockedCompareExchange64(dst, desired, expectation);
+    return *expected == expectation;
+  }
 #else
-  return atomic_compare_exchange_strong_explicit(dst, expected, desired, memory_order_acq_rel, memory_order_acquire);
+  return atomic_compare_exchange_strong_explicit(
+      dst, expected, desired, memory_order_acq_rel, memory_order_acquire);
+#endif
+}
+
+dword_t gv_dword_atomic_xchg(_Atomic dword_t *dst, dword_t src) {
+  assert(dst != NULL);
+#if FORCE_CAS || defined(_WIN64)
+  {
+    dword_t old = gv_dword_new();
+    while (!gv_dword_atomic_cas(dst, &old, src)) {
+    }
+    return old;
+  }
+#elif defined(_WIN32)
+  return _InterlockedExchange64(dst, src);
 #endif
 }
--- lib/util/dword.h
+++ lib/util/dword.h
@@ -1,26 +1,32 @@
 #pragma once
 
-#include <util/api.h>
-#include <stdbool.h>
 #include <stdatomic.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <util/api.h>
 
 #if defined(__SIZEOF_POINTER__) && __SIZEOF_POINTER__ == 4 // e.g GCC on x86
 typedef uint64_t dword_t;
-#elif defined(__SIZEOF_POINTER__) && __SIZEOF_POINTER__ == 8 // e.g. GCC on x86-64
+#elif defined(__SIZEOF_POINTER__) &&                                           \
+    __SIZEOF_POINTER__ == 8 // e.g. GCC on x86-64
 typedef unsigned __int128 dword_t;
-#elif defined(_WIN64) // MSVC on 64-bit ARM, x64, ARM64EC
-typedef struct { uint64_t lo, hi; } dword_t;
-#elif defined(_WIN32) // MSVC on 32-bit ARM, x86
-typedef uint64_t dword_t;
+#elif defined(_WIN64)       // MSVC on 64-bit ARM, x64, ARM64EC
+typedef struct __declspec(align(16)) {
+  int64_t word[2];
+} dword_t;
+#elif defined(_WIN32)       // MSVC on 32-bit ARM, x86
+typedef __declspec(align(8)) int64_t dword_t;
 #else
 #error "unknown platform pointer size"
 #endif
 
-UTIL_API dword_t dword_new(void);
+UTIL_API dword_t gv_dword_new(void);
 
-UTIL_API dword_t dword_atomic_load(_Atomic dword_t *src);
+UTIL_API dword_t gv_dword_atomic_load(_Atomic dword_t *src);
 
-UTIL_API void dword_atomic_store(_Atomic dword_t *dst, dword_t src);
+UTIL_API void gv_dword_atomic_store(_Atomic dword_t *dst, dword_t src);
 
-UTIL_API bool dword_atomic_cas(_Atomic dword_t *dst, dword_t *expected, dword_t desired);
+UTIL_API bool gv_dword_atomic_cas(_Atomic dword_t *dst, dword_t *expected,
+                                  dword_t desired);
+
+UTIL_API dword_t gv_dword_atomic_xchg(_Atomic dword_t *dst, dword_t src);
