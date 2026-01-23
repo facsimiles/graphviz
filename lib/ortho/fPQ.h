2852eb7e1d810b777cac0656a418cedb9283ac8a
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-01-23 17:47:38 +1100

    ortho: clang-format fPQ.h

--- lib/ortho/fPQ.h
+++ lib/ortho/fPQ.h
@@ -4,7 +4,7 @@
  */
 
 /*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property 
+ * Copyright (c) 2011 AT&T Intellectual Property
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,7 +34,7 @@ void PQfree(pq_t *pq);
 void PQinit(pq_t *pq);
 
 /// @param pq Priority queue to insert into
-int PQ_insert(pq_t *pq, snode* np);
+int PQ_insert(pq_t *pq, snode *np);
 
 /// @param pq Priority queue to pop
 snode *PQremove(pq_t *pq);
