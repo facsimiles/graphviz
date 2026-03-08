58a6911836f6d30cef5327b8da3fe5d31abd0955
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-03-08 12:21:17 -0700

    common dotneato_closest: reduce scope of 'high'

--- lib/common/utils.c
+++ lib/common/utils.c
@@ -346,7 +346,7 @@ bool mapbool(const char *p)
 
 pointf dotneato_closest(splines * spl, pointf pt)
 {
-    double high, t;
+    double t;
     pointf pt2;
     bezier bz;
 
@@ -376,8 +376,8 @@ pointf dotneato_closest(splines * spl, pointf pt)
     const size_t j = 3 * (OPTIONAL_VALUE(bestj) / 3);
     const pointf c[] =
       {bz.list[j], bz.list[j + 1], bz.list[j + 2], bz.list[j + 3]};
-    high = 1.0;
-    for (double low = 0, dlow2 = DIST2(c[0], pt), dhigh2 = DIST2(c[3], pt);;) {
+    for (double low = 0, high = 1.0, dlow2 = DIST2(c[0], pt),
+         dhigh2 = DIST2(c[3], pt);;) {
 	t = (low + high) / 2.0;
 	pt2 = Bezier(c, t, NULL, NULL);
 	if (fabs(dlow2 - dhigh2) < 1.0)
