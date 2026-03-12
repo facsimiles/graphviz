accf75134ea9b3d9b1d0b8dd3b4e49c6e05bb510
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-03-11 17:02:36 -0700

    sfdpgen ideal_distance_matrix: reduce scope of 'ia'

--- lib/sfdpgen/post_process.c
+++ lib/sfdpgen/post_process.c
@@ -37,13 +37,12 @@ static SparseMatrix ideal_distance_matrix(SparseMatrix A, int dim, double *x){
   /* find the ideal distance between edges, either 1, or |N[i] \Union N[j]| - |N[i] \Intersection N[j]|
    */
   SparseMatrix D;
-  int *ia;
   double sum;
 
   assert(SparseMatrix_is_symmetric(A, false));
 
   D = SparseMatrix_copy(A);
-  ia = D->ia;
+  const int *const ia = D->ia;
   const int *const ja = D->ja;
   if (D->type != MATRIX_TYPE_REAL){
     free(D->a);
