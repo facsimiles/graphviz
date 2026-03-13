77c8ecf75c325b7f85638b8ce7f2a31ed77fc44e
Brian R. Murphy <brianrmurphy@gmail.com>
2026-03-12 23:28:25 -0700

    add a test for LIST_COPY
    Try a few element sizes and various target scenarios, also check that destructors are copied.

--- lib/util/test_list.c
+++ lib/util/test_list.c
@@ -8,10 +8,12 @@
 #define NO_CONFIG // suppress include of config.h in list.c
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <util/list.c>
 #include <util/list.h>
+#include <util/prisize_t.h>
 #include <util/unused.h>
 
 // test construction and destruction, with nothing in-between
@@ -624,6 +626,211 @@ static void test_remove_with_dtor(void) {
   LIST_FREE(&xs);
 }
 
+// Helpers for copy tests
+
+typedef LIST(int) int_list_t;
+typedef LIST(char) char_list_t;
+
+#define DEFINE_LIST_COMPARE(elt_type)                                          \
+  static bool elt_type##_list_compare(elt_type##_list_t *l1,                   \
+                                      elt_type##_list_t *l2) {                 \
+    if (LIST_SIZE(l1) != LIST_SIZE(l2)) {                                      \
+      fprintf(stderr, "sizes %" PRISIZE_T " and %" PRISIZE_T " don't match\n", \
+              LIST_SIZE(l1), LIST_SIZE(l2));                                   \
+      return false;                                                            \
+    }                                                                          \
+    for (size_t i = 0; i < LIST_SIZE(l1); ++i) {                               \
+      if (LIST_GET(l1, i) != LIST_GET(l2, i)) {                                \
+        fprintf(stderr,                                                        \
+                "elements l1(%" PRISIZE_T ")=%d != l2(%" PRISIZE_T ")=%d\n",   \
+                i, (int)LIST_GET(l1, i), i, (int)LIST_GET(l2, i));             \
+        return false;                                                          \
+      }                                                                        \
+    }                                                                          \
+    assert(l1->dtor == l2->dtor);                                              \
+    return true;                                                               \
+  }									       \
+  _Static_assert(true, "")
+
+DEFINE_LIST_COMPARE(int);
+DEFINE_LIST_COMPARE(char);
+
+static int int_dtor_1_count = 0;
+static int int_dtor_1_sum = 0;
+
+static void int_dtor_1(int x) {
+  ++int_dtor_1_count;
+  int_dtor_1_sum += x;
+}
+
+static int int_dtor_2_count = 0;
+static int int_dtor_2_sum = 0;
+
+static void int_dtor_2(int x) {
+  ++int_dtor_2_count;
+  int_dtor_2_sum += x;
+}
+
+/// Test that copy doesn't delete destination elements.
+static void test_copy_dtor(void) {
+  int_list_t xs = {.dtor = int_dtor_1};
+  for (int i = 5; i > 0; --i) {
+    LIST_APPEND(&xs, i);
+  }
+  int_list_t ys = {.dtor = int_dtor_2};
+  for (int i = 0; i < 3; ++i) {
+    LIST_APPEND(&ys, i);
+  }
+  LIST_COPY(&ys, &xs);
+  assert(int_list_compare(&xs, &ys));
+  // Copy doesn't delete items in target.
+  assert(int_dtor_2_count == 0);
+  assert(int_dtor_2_sum == 0);
+
+  LIST_FREE(&xs);
+  assert(int_dtor_1_count == 5);
+  assert(int_dtor_1_sum == 15);
+  LIST_FREE(&ys);
+  assert(int_dtor_1_count == 10);
+  assert(int_dtor_1_sum == 30);
+
+  // Still not deleted original ys
+  assert(int_dtor_2_count == 0);
+  assert(int_dtor_2_sum == 0);
+}
+
+#define MULTI_TARGET_COPY_SCENARIOS(elt_type)                                  \
+  static bool multi_target_##elt_type##_copy_scenarios(                        \
+      elt_type##_list_t *input) {                                              \
+    elt_type##_list_t empty_copy = {0};                                        \
+    LIST_COPY(&empty_copy, input);                                             \
+    if (!elt_type##_list_compare(&empty_copy, input)) {                        \
+      fprintf(stderr, "scenario 1 (empty_copy) failed");                       \
+      return false;                                                            \
+      LIST_FREE(&empty_copy);                                                  \
+    }                                                                          \
+                                                                               \
+    LIST_COPY(&empty_copy, input);                                             \
+    if (!elt_type##_list_compare(&empty_copy, input)) {                        \
+      fprintf(stderr, "scenario 2 (double copy) failed");                      \
+      LIST_FREE(&empty_copy);                                                  \
+      return false;                                                            \
+    }                                                                          \
+    LIST_FREE(&empty_copy);                                                    \
+                                                                               \
+    elt_type##_list_t misc_list = {0};                                         \
+    LIST_APPEND(&misc_list, 39);                                               \
+    LIST_APPEND(&misc_list, 43);                                               \
+    LIST_PREPEND(&misc_list, 17);                                              \
+    LIST_COPY(&misc_list, input);                                              \
+    if (!elt_type##_list_compare(&misc_list, input)) {                         \
+      fprintf(stderr, "scenario 3 (copy onto small list) failed");             \
+      LIST_FREE(&misc_list);                                                   \
+      return false;                                                            \
+    }                                                                          \
+    LIST_FREE(&misc_list);                                                     \
+                                                                               \
+    elt_type##_list_t big_list = {0};                                          \
+    for (int i = 0; i < 2 * (int)LIST_SIZE(input); ++i) {                      \
+      LIST_APPEND(&big_list, 3 + i);                                           \
+    }                                                                          \
+    LIST_COPY(&big_list, input);                                               \
+    if (!elt_type##_list_compare(&big_list, input)) {                          \
+      fprintf(stderr, "scenario 4 (copy onto big list) failed");               \
+      LIST_FREE(&big_list);                                                    \
+      return false;                                                            \
+    }                                                                          \
+    LIST_FREE(&big_list);                                                      \
+    return true;                                                               \
+  }									       \
+  _Static_assert(true, "")
+
+#define SIMPLE_COPY_TEST(elt_type)                                             \
+  static void test_copy_simple_##elt_type(void) {                              \
+    elt_type##_list_t list1 = {0};                                             \
+    for (elt_type i = 0; i < 8; ++i) {                                         \
+      LIST_PUSH_BACK(&list1, i);                                               \
+    }                                                                          \
+    assert(multi_target_##elt_type##_copy_scenarios(&list1));                  \
+    LIST_FREE(&list1);                                                         \
+                                                                               \
+    elt_type##_list_t list2 = {0};                                             \
+    for (elt_type i = 0; i < 8; ++i) {                                         \
+      LIST_PUSH_BACK(&list2, i);                                               \
+    }                                                                          \
+    for (elt_type i = 0; i < 8; ++i) {                                         \
+      LIST_PREPEND(&list2, 8 + i);                                             \
+    }                                                                          \
+    assert(multi_target_##elt_type##_copy_scenarios(&list2));                  \
+    LIST_FREE(&list2);                                                         \
+                                                                               \
+    elt_type##_list_t list3 = {0};                                             \
+    for (elt_type i = 0; i < 8; ++i) {                                         \
+      LIST_PREPEND(&list3, 8 + i);                                             \
+    }                                                                          \
+    assert(multi_target_##elt_type##_copy_scenarios(&list3));                  \
+    LIST_FREE(&list3);                                                         \
+  }									       \
+  _Static_assert(true, "")
+
+MULTI_TARGET_COPY_SCENARIOS(int);
+MULTI_TARGET_COPY_SCENARIOS(char);
+
+SIMPLE_COPY_TEST(int);
+SIMPLE_COPY_TEST(char);
+
+/// Test copy with various gaps in the source list.
+static void test_copy_reserve(void) {
+  // With knowledge of the representation,
+  // we are use `LIST_RESERVE` to control
+  // the created representation to exercise
+  // several buffer fill scenarios that
+  // lead to different copy paths.
+
+  // contents at start of buffer, gap at end
+  int_list_t list1 = {0};
+  LIST_RESERVE(&list1, 16);
+  // list1.impl.capacity = 16
+  for (int i = 0; i < 8; ++i) {
+    LIST_PUSH_BACK(&list1, i);
+  }
+  // list1.base[] = { 0, 1, 2, .., 7, 0, 0, .. 0 }
+  // list1.impl.head = 0
+  // list1.impl.size = 8
+  assert(multi_target_int_copy_scenarios(&list1));
+  LIST_FREE(&list1);
+
+  // contents filling buffer completely,
+  // but with head in middle
+  // will have 15,..,8,0..7 [filling buffer]
+  int_list_t list2 = {0};
+  LIST_RESERVE(&list2, 16);
+  // list1.impl.capacity = 16
+  for (int i = 0; i < 8; ++i) {
+    LIST_PUSH_BACK(&list2, i);
+  }
+  for (int i = 0; i < 8; ++i) {
+    LIST_PREPEND(&list2, 8 + i);
+  }
+  // list2.base[] = { 0, 1, .., 7, 15, 14, .., 8 }
+  // list2.impl.head = 8
+  assert(multi_target_int_copy_scenarios(&list2));
+  LIST_FREE(&list2);
+
+  // contents at end of buffer, with gap in front
+  // will have 15,..,8 [packed at end of buffer]
+  int_list_t list3 = {0};
+  LIST_RESERVE(&list3, 16);
+  // list1.impl.capacity = 16
+  for (int i = 0; i < 8; ++i) {
+    LIST_PREPEND(&list3, 8 + i);
+  }
+  // list3.base[] = { 0, 0, .., 0, 15, 14, .., 8 }
+  // list2.impl.head = 8
+  assert(multi_target_int_copy_scenarios(&list3));
+  LIST_FREE(&list3);
+}
+
 #ifndef BAD_TEST
 #define BAD_TEST 0
 #endif
@@ -789,6 +996,10 @@ int main(void) {
   RUN(dtor);
   RUN(remove_with_dtor);
   RUN(zero_item_size);
+  RUN(copy_dtor);
+  RUN(copy_reserve);
+  RUN(copy_simple_int);
+  RUN(copy_simple_char);
 
 #undef RUN
 
