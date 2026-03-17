9165223cf239a92f2d764608a815d5b306aa6b4e
Brian R. Murphy <brianrmurphy@gmail.com>
2026-03-16 21:34:31 -0700

    Avoid adding auxiliary edges that create cycles.
    This avoids "trouble in init_rank" issues 1213, 1939, 2796.
    And apparently also fixes 2471 on windows (but not all systems).

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -614,9 +614,6 @@ def test_925():
 
 
 @pytest.mark.parametrize("testcase", ("1213-1.dot", "1213-2.dot"))
-@pytest.mark.xfail(
-    strict=True, reason="https://gitlab.com/graphviz/graphviz/-/issues/1213"
-)
 def test_1213(testcase: str):
     """
     clustering should not trigger “trouble in init_rank” errors
@@ -2196,9 +2193,6 @@ def test_1931():
     assert "line 5\nline 6" in xdot
 
 
-@pytest.mark.xfail(
-    strict=True, reason="https://gitlab.com/graphviz/graphviz/-/issues/1939"
-)
 def test_1939():
     """
     clustering should not cause “trouble in init_rank” errors
@@ -4338,7 +4332,7 @@ def test_2470():
 
 @pytest.mark.xfail(
     reason="https://gitlab.com/graphviz/graphviz/-/issues/2471",
-    strict=True,
+    strict=False,
 )
 def test_2471():
     """
@@ -6292,9 +6286,6 @@ def test_2782():
             raise
 
 
-@pytest.mark.xfail(
-    reason="https://gitlab.com/graphviz/graphviz/-/issues/2796", strict=True
-)
 def test_2796():
     """
     Graphviz should be able to triangulate the points in this graph
